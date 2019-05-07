7630c9abcb0fb5962f0ed5e16a44a66ff31cd27b
david <davidhunt@mitre.org>
2019-05-07 11:09:11 -0400

    ensuring commands are running under obfuscation

--- app/service/utility_svc.py
+++ app/service/utility_svc.py
@@ -1,8 +1,15 @@
 from base64 import b64encode, b64decode
 from random import randint
 
+from app.utility.stealth import obfuscate_ps1, obfuscate_bash
+
 
 class UtilityService:
+
+    def obfuscate(self, executor, code):
+        options = dict(psh=lambda c: self.encode_string(obfuscate_ps1(self.decode_bytes(c))),
+                       bash=lambda c: obfuscate_bash(c))
+        return options[executor](code)
 
     @staticmethod
     def decode_bytes(s):
---
+++ app/utility/stealth.py
@@ -0,0 +1,32 @@
+import string
+
+from base64 import b64encode
+from random import randint, choice
+
+
+def obfuscate_ps1(code):
+    ran1 = ''.join([choice(string.ascii_letters + string.digits) for x in range(3)])
+    ran2 = ''.join([choice(string.ascii_letters + string.digits) for x in range(3)])
+    ran3 = ''.join([choice(string.ascii_letters + string.digits) for x in range(3)])
+
+    avblah = b64encode(code.encode('utf_16_le'))
+    avsux = randint(4000, 5000)
+    avnotftw = [avblah[i: i + avsux] for i in range(0, len(avblah), avsux)]
+    haha_av = ''
+    counter = 0
+    for non_signature in avnotftw:
+        non_signature = (non_signature.rstrip())
+        if counter > 0:
+            haha_av = haha_av + '+'
+            haha_av = haha_av + "'"
+        surprise_surprise = non_signature.decode('ascii') + "'"
+        haha_av = haha_av + surprise_surprise
+        haha_av = haha_av.replace("==", "'+'==")
+        counter = 1
+    mangle_quotes = (choice(["''"]))
+    return '''powershell /w 1 /C "s{0}v {1} -;s{0}v {2} e{0}c;s{0}v {3} ((g{0}v {4}).value.toString()+(g{0}v {5}).value.toString());powershell (g{0}v {6}).value.toString() (\''''.format(
+        mangle_quotes, ran1, ran2, ran3, ran1, ran2, ran3) + haha_av + ")" + '"'
+
+
+def obfuscate_bash(code):
+    return code
