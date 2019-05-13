8b87bbd27a99ba385b052f5162531255f36aef4b
Caleb Little <50213210+ArtificialErmine@users.noreply.github.com>
2019-05-13 07:43:08 -0400

    virts-156 (#131)
    * Initial hookups for virts-156/virts-163
    
    * Minor tweaks to the op_control code to support additional use cases
    
    * Minor tweaks to operation handling
    
    * Rebased to deal with changes
    
    * Minor changes to address merge comments

--- app/service/operation_svc.py
+++ app/service/operation_svc.py
@@ -4,11 +4,13 @@ from datetime import datetime
 from importlib import import_module
 
 from app.utility.logger import Logger
+from app.utility.op_control import OpControl
 
 
-class OperationService:
+class OperationService(OpControl):
 
     def __init__(self, data_svc, utility_svc, planner):
+        super().__init__(data_svc.dao)
         self.data_svc = data_svc
         self.utility_svc = utility_svc
         self.loop = asyncio.get_event_loop()
@@ -20,6 +22,11 @@ class OperationService:
         for op in await self.data_svc.dao.get('core_operation'):
             if not op['finish']:
                 self.loop.create_task(self.run(op['id']))
+
+    async def close_operation(self, op_id):
+        self.log.debug('Operation complete: %s' % op_id)
+        update = dict(finish=datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
+        await self.data_svc.dao.update('core_operation', key='id', value=op_id, data=update)
 
     async def run(self, op_id):
         self.log.debug('Starting operation: %s' % op_id)
@@ -33,9 +40,7 @@ class OperationService:
                 operation = await self.data_svc.explode_operation(dict(id=op_id))
             if operation[0]['cleanup']:
                 await self.cleanup(op_id)
-            self.log.debug('Operation complete: %s' % op_id)
-            update = dict(finish=datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
-            await self.data_svc.dao.update('core_operation', key='id', value=op_id, data=update)
+            await self.close_operation(op_id)
         except Exception:
             traceback.print_exc()
 
@@ -46,3 +51,4 @@ class OperationService:
             link = dict(op_id=c['op_id'], host_id=c['agent_id'], ability_id=c['ability_id'], decide=datetime.now(),
                         command=c['command'], score=0, jitter=1)
             await self.data_svc.create_link(link)
+        await self.cleanup_operation(op_id)
