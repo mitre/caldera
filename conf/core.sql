3d11a11277ecf2d1c66862c37357181255d534b3
wbooth <18699738+wbooth@users.noreply.github.com>
2019-07-02 19:23:55 -0400

    add core_agent_map id and pass through to explode_agents

--- conf/core.sql
+++ conf/core.sql
@@ -2,7 +2,7 @@ CREATE TABLE if not exists core_result (link_id integer, output text, parsed dat
 CREATE TABLE if not exists core_ability (id integer primary key AUTOINCREMENT, ability_id text, technique integer, name text, test text, description text, cleanup text, platform, UNIQUE (ability_id, platform) ON CONFLICT IGNORE);
 CREATE TABLE if not exists core_payload (id integer primary key AUTOINCREMENT, ability integer, payload text, UNIQUE (ability, payload) ON CONFLICT IGNORE);
 CREATE TABLE if not exists core_group (id integer primary key AUTOINCREMENT, name text, deactivated date DEFAULT 0, UNIQUE(name, deactivated));
-CREATE TABLE if not exists core_group_map (group_id integer, agent_id integer, UNIQUE(group_id, agent_id));
+CREATE TABLE if not exists core_group_map (id integer primary key AUTOINCREMENT, group_id integer, agent_id integer, UNIQUE(group_id, agent_id));
 CREATE TABLE if not exists core_adversary (id integer primary key AUTOINCREMENT, name text, description text, UNIQUE (name));
 CREATE TABLE if not exists core_adversary_map (phase integer, adversary_id integer, ability_id text, UNIQUE (adversary_id, phase, ability_id));
 CREATE TABLE if not exists core_operation (id integer primary key AUTOINCREMENT, name text, host_group integer, adversary integer, jitter text, start date, finish date, phase integer, cleanup integer, stealth integer);
