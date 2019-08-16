9ed3255b799a2466f10630d44da481924e006442
david <davidhunt@mitre.org>
2019-08-16 15:55:18 -0400

    moving generic functions to agent_svc

--- app/service/agent_svc.py
+++ app/service/agent_svc.py
@@ -1,3 +1,4 @@
+import json
 from datetime import datetime
 
 
@@ -19,3 +20,29 @@ class AgentService:
             queued = dict(last_seen=now, paw=paw, platform=platform, server=server, host_group=group)
             await self.data_svc.create_agent(agent=queued)
             return (await self.data_svc.explode_agents(criteria=dict(paw=paw)))[0]
+
+    async def get_instructions(self, paw):
+        commands = await self.data_svc.explode_chain(criteria=dict(paw=paw))
+        instructions = []
+        for link in [c for c in commands if not c['collect']]:
+            await self.data_svc.update('core_chain', key='id', value=link['id'], data=dict(collect=datetime.now()))
+            payload = await self._gather_payload(link['ability'])
+            instructions.append(json.dumps(dict(id=link['id'],
+                                                sleep=link['jitter'],
+                                                command=link['command'],
+                                                cleanup=link['cleanup'],
+                                                payload=payload)))
+        return json.dumps(instructions)
+
+    async def save_results(self, link_id, output, status):
+        finished = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+        await self.data_svc.create_result(result=dict(link_id=link_id, output=output))
+        await self.data_svc.update('core_chain', key='id', value=link_id,
+                                   data=dict(status=int(status), finish=finished))
+        return json.dumps(dict(status=True))
+
+    """ PRIVATE """
+
+    async def _gather_payload(self, ability_id):
+        payload = await self.data_svc.explode_payloads(criteria=dict(ability=ability_id))
+        return payload[0]['payload'] if payload else ''
