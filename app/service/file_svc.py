3ac5c416d6eecbc7b7523b364f4fdbd3b3217462
Adam Lee <ajunlee@gmail.com>
2019-09-19 23:28:48 +0800

    Fix OS command injection in file_svc (#481)
    * get platform value from abilities data service
    
    * add async init in file_svc and call it from server.py
    
    * add _verify_check in download function
    
    * remove extra line in _compile and add newline at the end of file
    
    * remote extra lower() of platform in _compile

--- app/service/file_svc.py
+++ app/service/file_svc.py
@@ -17,6 +17,7 @@ class FileSvc(BaseService):
         self.plugins = plugins
         self.exfil_dir = exfil_dir
         self.log = self.add_service('file_svc', self)
+        self.data_svc = self.get_service('data_svc')
 
     async def download(self, request):
         """
@@ -86,7 +87,10 @@ class FileSvc(BaseService):
 
     async def _get_file(self, name, platform):
         if name.endswith('.go'):
-            name = await self._go_compile(name, platform)
+            if not platform or platform.lower() not in {ab['platform'].lower() for ab in await self.data_svc.explode_abilities()}:
+                # platform not found
+                raise FileNotFoundError
+            name = await self._go_compile(name, platform.lower())
             _, file_path = await self.find_file_path(name, location='payloads')
             with open(file_path, 'rb') as file_stream:
                 return name, file_stream.read()
@@ -103,16 +107,14 @@ class FileSvc(BaseService):
         raise FileNotFoundError
 
     async def _go_compile(self, name, platform):
-        if name.endswith('.go'):
-            if which('go') is not None:
-                plugin, file_path = await self.find_file_path(name)
-                await self._change_file_hash(file_path)
-                output = 'plugins/%s/payloads/%s-%s' % (plugin, name, platform)
-                os.system('GOOS=%s go build -o %s -ldflags="-s -w" %s' % (platform, output, file_path))
-                self.log.debug('%s compiled for %s with MD5=%s' %
-                               (name, platform, md5(open(output, 'rb').read()).hexdigest()))
-            return '%s-%s' % (name, platform)
-        return name
+        if which('go') is not None:
+            plugin, file_path = await self.find_file_path(name)
+            await self._change_file_hash(file_path)
+            output = 'plugins/%s/payloads/%s-%s' % (plugin, name, platform)
+            os.system('GOOS=%s go build -o %s -ldflags="-s -w" %s' % (platform, output, file_path))
+            self.log.debug('%s compiled for %s with MD5=%s' %
+                           (name, platform, md5(open(output, 'rb').read()).hexdigest()))
+        return '%s-%s' % (name, platform)
 
     @staticmethod
     async def _change_file_hash(file_path, size=30):
