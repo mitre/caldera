01f08008ce56d316b9e596e5107d4160fb98b132
david <davidhunt@mitre.org>
2019-09-25 15:48:10 -0400

    ensuring adversaries can be chained together

--- app/service/data_svc.py
+++ app/service/data_svc.py
@@ -47,7 +47,7 @@ class DataService(BaseService):
         """
         for filename in glob.iglob('%s/*.yml' % directory, recursive=True):
             for adv in self.strip_yml(filename):
-                phases = [dict(phase=k, id=i) for k, v in adv.get('phases').items() for i in v]
+                phases = [dict(phase=k, id=i) for k, v in adv.get('phases', dict()).items() for i in v]
                 for pack in [await self._add_adversary_packs(p) for p in adv.get('packs', [])]:
                     phases += pack
                 if adv['visible']:
