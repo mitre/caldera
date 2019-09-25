693c5e930a3fb2155f0eccf95543a45aa2e72ec4
david <davidhunt@mitre.org>
2019-09-25 15:35:02 -0400

    ensuring adversaries can be chained together

--- app/service/data_svc.py
+++ app/service/data_svc.py
@@ -47,8 +47,11 @@ class DataService(BaseService):
         """
         for filename in glob.iglob('%s/*.yml' % directory, recursive=True):
             for adv in self.strip_yml(filename):
-                phases = [dict(phase=k, id=i) for k, v in adv['phases'].items() for i in v]
-                await self.create_adversary(adv['id'], adv['name'], adv['description'], phases)
+                phases = [dict(phase=k, id=i) for k, v in adv.get('phases').items() for i in v]
+                for pack in [await self._add_adversary_packs(p) for p in adv.get('packs', [])]:
+                    phases += pack
+                if adv['visible']:
+                    await self.create_adversary(adv['id'], adv['name'], adv['description'], phases)
 
     async def load_facts(self, directory):
         """
@@ -402,3 +405,8 @@ class DataService(BaseService):
                     await self.dao.delete('core_ability', dict(id=saved['id']))
             if saved['platform'] not in ability['platforms']:
                 await self.dao.delete('core_ability', dict(id=saved['id']))
+
+    async def _add_adversary_packs(self, pack):
+        _, filename = await self.get_service('file_svc').find_file_path('%s.yml' % pack, location='data')
+        for adv in self.strip_yml(filename):
+            return [dict(phase=k, id=i) for k, v in adv.get('phases').items() for i in v]
