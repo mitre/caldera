7eae023dad9a5e855671e0a088a5686d200756f1
Cervando Banuelos <cbanuelos@aggienetwork.com>
2019-10-15 15:53:26 -0400

    virts-509-update agent executors on heartbeat If agent heartbeat now has changed executors and/or priorities for executors, handle_heartbeat function now updates the executors in the database.

--- app/service/agent_svc.py
+++ app/service/agent_svc.py
@@ -72,7 +72,14 @@ class AgentService(BaseService):
             update_data = dict(last_seen=now, pid=pid, ppid=ppid)
             if agent[0]['trusted']:
                 update_data['last_trusted_seen'] = now
-            await self.data_svc.update('core_agent', 'paw', paw, data=update_data)
+            await self.get_service('data_svc').update('core_agent', 'paw', paw, data=update_data)
+            incoming_executors =[]
+            for i,e in enumerate(executors):
+                incoming_executors.append(dict(executor=e, preferred=1 if i == 0 else 0))
+            if agent[0]['executors'] != incoming_executors:
+                await self.get_service('data_svc').delete('core_executor', dict(agent_id=agent[0]['id']))
+                for item in incoming_executors:
+                    await self.get_service('data_svc').create('core_executor', dict(agent_id=agent[0]['id'], executor=item['executor'], preferred=item['preferred']))
         else:
             queued = dict(last_seen=now, paw=paw, platform=platform, server=server, host_group=group,
                           location=location, architecture=architecture, pid=pid, ppid=ppid,
