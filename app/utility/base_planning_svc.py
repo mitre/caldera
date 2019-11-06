94b83cd46047a16fce48b60c2db08a47c45c514e
wbooth <18699738+wbooth@users.noreply.github.com>
2019-11-06 17:41:53 -0500

    Bug Fix: Base Planning Service Used Attribute (#726)

--- app/utility/base_planning_svc.py
+++ app/utility/base_planning_svc.py
@@ -132,7 +132,7 @@ class BasePlanningService(BaseService):
         enforce any defined requirements on the link
         """
         for req_inst in ability_requirements:
-            uf = link.get('used', [])
+            uf = link.used
             requirements_info = dict(module=req_inst.module, enforcements=req_inst.relationships[0])
             requirement = await self.load_module('Requirement', requirements_info)
             if not requirement.enforce(combo[0], uf, operation.facts):
