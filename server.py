dc1ac2f5c639b08687573dd7bbc297bc7eb922e3
Alexander Manners <43142871+khyberspache@users.noreply.github.com>
2019-11-12 23:40:08 -0500

    fix for old python versions (#753)
    * fix for old python versions
    
    * removing blank spaces
    
    * a different approache

--- server.py
+++ server.py
@@ -21,11 +21,12 @@ from app.service.planning_svc import PlanningService
 
 
 async def background_tasks(app):
-    asyncio.create_task(application.start_sniffer_untrusted_agents())
-    asyncio.create_task(application.resume_operations())
-    asyncio.create_task(data_svc.load_data(directory='data'))
-    asyncio.create_task(data_svc.restore_state())
-    asyncio.create_task(application.run_scheduler())
+    loop = asyncio.get_event_loop()
+    loop.create_task(application.start_sniffer_untrusted_agents())
+    loop.create_task(application.resume_operations())
+    loop.create_task(data_svc.load_data(directory='data'))
+    loop.create_task(data_svc.restore_state())
+    loop.create_task(application.run_scheduler())
 
 
 def build_plugins(plugs):
