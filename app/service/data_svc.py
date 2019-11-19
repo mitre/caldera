00a8afc39c5cf52e6dc9fa95470b7090f9f37c2c
wbooth <18699738+wbooth@users.noreply.github.com>
2019-11-19 17:47:42 -0500

    Missing Adversary Abilities Warning (#783)
    * Missing Adversary Abilities Warning
    
    ```ERROR:data_svc:Missing Ability (99999999-e342-418a-a9b0-002c483e0fa6) for adversary: Cyber```
    
    * Update data_svc.py

--- app/service/data_svc.py
+++ app/service/data_svc.py
@@ -116,7 +116,10 @@ class DataService(BaseService):
                 if adv.get('visible', True):
                     pp = defaultdict(list)
                     for phase in phases:
-                        for ability in await self.locate('abilities', match=dict(ability_id=phase['id'])):
+                        matching_abilities = await self.locate('abilities', match=dict(ability_id=phase['id']))
+                        if not len(matching_abilities):
+                            self.log.error('Missing Ability (%s) for adversary: %s' % (phase['id'], adv['name']))
+                        for ability in matching_abilities:
                             pp[phase['phase']].append(ability)
                     phases = dict(pp)
                     await self.store(
