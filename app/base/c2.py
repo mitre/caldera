1cee479c4deee70acdf9190dd28ba5ee85d182b1
Hunt, David <davidhunt@mitre.org>
2019-11-19 14:49:12 -0500

    test

---
+++ app/base/c2.py
@@ -0,0 +1,87 @@
+import asyncio
+import json
+
+from datetime import datetime
+
+from app.objects.c_agent import Agent
+
+
+class C2:
+
+    def __init__(self, services):
+        self.data_svc = services.get('data_svc')
+        self.loop = asyncio.get_event_loop()
+        self.log = services.get('app_svc').create_logger('c2')
+
+    async def handle_heartbeat(self, paw, platform, server, group, host, username, executors, architecture, location,
+                               pid, ppid, sleep, privilege):
+        """
+        Accept all components of an agent profile and save a new agent or register an updated heartbeat.
+        :param paw:
+        :param platform:
+        :param server:
+        :param group:
+        :param host:
+        :param username:
+        :param executors:
+        :param architecture:
+        :param location:
+        :param pid:
+        :param ppid:
+        :param sleep:
+        :param privilege:
+        :return: the agent object from explode
+        """
+        self.log.debug('HEARTBEAT (%s)' % paw)
+        agent = Agent(paw=paw, host=host, username=username, platform=platform, server=server, location=location,
+                      executors=executors, architecture=architecture, pid=pid, ppid=ppid, privilege=privilege)
+        if await self.data_svc.locate('agents', dict(paw=paw)):
+            return await self.data_svc.store(agent)
+        agent.sleep_min = agent.sleep_max = sleep
+        agent.group = group
+        agent.trusted = True
+        return await self.data_svc.store(agent)
+
+    async def get_instructions(self, paw):
+        """
+        Get next set of instructions to execute
+        :param paw:
+        :return: a list of links in JSON format
+        """
+        ops = await self.data_svc.locate('operations', match=dict(finish=None))
+        instructions = []
+        for link in [c for op in ops for c in op.chain
+                     if c.paw == paw and not c.collect and c.status == c.states['EXECUTE']]:
+            link.collect = datetime.now()
+            payload = link.ability.payload if link.ability.payload else ''
+            instructions.append(json.dumps(dict(id=link.unique,
+                                                sleep=link.jitter,
+                                                command=link.command,
+                                                executor=link.ability.executor,
+                                                payload=payload)))
+        return json.dumps(instructions)
+
+    async def save_results(self, id, output, status, pid):
+        """
+        Save the results from a single executed link
+        :param id:
+        :param output:
+        :param status:
+        :param pid:
+        :return: a JSON status message
+        """
+        try:
+            for op in await self.data_svc.locate('operations', match=dict(finish=None)):
+                link = next((l for l in op.chain if l.unique == id), None)
+                if link:
+                    link.pid = int(pid)
+                    link.finish = self.data_svc.get_current_timestamp()
+                    link.status = int(status)
+                    if output:
+                        with open('data/results/%s' % id, 'w') as out:
+                            out.write(output)
+                        self.loop.create_task(link.parse(op))
+                    await self.data_svc.store(Agent(paw=link.paw))
+                    return json.dumps(dict(status=True))
+        except Exception as e:
+            self.log.error('[!] save_results: %s' % e)
