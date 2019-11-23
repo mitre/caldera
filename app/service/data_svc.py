7d030d54cfd4ca8f4f10f8e0ccb47b40e693bc01
Hunt, David <davidhunt@mitre.org>
2019-11-22 19:14:41 -0500

    ensuring apply function verifies original schema

--- app/service/data_svc.py
+++ app/service/data_svc.py
@@ -58,8 +58,9 @@ class DataService(BaseService):
             with open('data/object_store', 'rb') as objects:
                 ram = pickle.load(objects)
                 for key in ram.keys():
-                    for c_object in ram[key]:
-                        await self.store(c_object)
+                    if key in self.schema:
+                        for c_object in ram[key]:
+                            await self.store(c_object)
             self.log.debug('Restored objects from persistent storage')
         self.log.debug('There are %s jobs in the scheduler' % len(self.ram['schedules']))
 
