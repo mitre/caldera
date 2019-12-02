d78013edcd12ddcd1e7c86b4ec2b8624f123156a
wbooth <18699738+wbooth@users.noreply.github.com>
2019-12-02 16:16:48 -0500

    Load Plugins In Same Order as Config

--- app/service/app_svc.py
+++ app/service/app_svc.py
@@ -111,9 +111,11 @@ class AppService(BaseService):
                 await self.get_service('data_svc').store(plugin)
                 if plugin.name in self.config['enabled_plugins']:
                     plugin.enabled = True
-        for plug in await self._services.get('data_svc').locate('plugins'):
-            if plug.name in self.config['enabled_plugins']:
-                await plug.enable(self.get_services())
+        plugins = await self._services.get('data_svc').locate('plugins')
+        for plugin in self.config['enabled_plugins']:
+            for plug in plugins:
+                if plugin == plug.name:
+                    await plug.enable(self.get_services())
 
         templates = ['plugins/%s/templates' % p.name.lower()
                      for p in await self.get_service('data_svc').locate('plugins')]
