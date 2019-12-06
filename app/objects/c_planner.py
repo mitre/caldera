705684638028b042d11f7053abf1c390947d49f1
Brian Edmonds <bedmonds@mitre.org>
2019-12-06 17:12:46 -0500

    updates after code review

--- app/objects/c_planner.py
+++ app/objects/c_planner.py
@@ -1,4 +1,5 @@
 from app.utility.base_object import BaseObject
+from app.objects.c_fact import Fact
 
 
 class Planner(BaseObject):
@@ -11,12 +12,16 @@ class Planner(BaseObject):
     def display(self):
         return dict(name=self.name, module=self.module, params=self.params, description=self.description)
 
-    def __init__(self, name, module, params, description=None):
+    def __init__(self, name, module, params, stopping_conditions=None, description=None):
         super().__init__()
         self.name = name
         self.module = module
         self.params = params
         self.description = description
+        self.stopping_conditions = []
+        if stopping_conditions:
+            self.stopping_conditions = [Fact(trait, value) for sc in stopping_conditions for trait, value in
+                                        sc.items()]
 
     def store(self, ram):
         existing = self.retrieve(ram['planners'], self.unique)
