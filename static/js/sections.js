16b73c3211193d31313d8f95c31e0473dcbe9201
Dan Kim <kimd@mm252646-pc.mitre.org>
2019-12-09 08:23:46 -0500

    Adjusting column filter to account for new column.

--- static/js/sections.js
+++ static/js/sections.js
@@ -174,7 +174,7 @@ $(document).ready(function () {
             {
                 extend: 'colvis',
                 text: 'Filter Columns', // Button text.
-                columns: ':lt(9)' // Keep last column (the remove agent column) as is.
+                columns: ':lt(10)' // Keep last column (the remove agent column) as is.
             }
         ],
         // Button, length-changer, pRocessing display element, filtering, table,
