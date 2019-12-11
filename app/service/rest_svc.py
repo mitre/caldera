e4329c5152584f472e4eac2a0be24c3a15bbdf62
david <49954156+privateducky@users.noreply.github.com>
2019-12-11 13:25:43 -0500

    adding functionality to allow deleting operations (#914)
    * adding functionality to allow deleting operations
    
    * adding functionality to allow deleting operations

--- app/service/rest_svc.py
+++ app/service/rest_svc.py
@@ -1,4 +1,5 @@
 import asyncio
+import glob
 import os
 import pathlib
 from collections import defaultdict
@@ -66,6 +67,14 @@ class RestService(BaseService):
 
     async def delete_agent(self, data):
         await self.get_service('data_svc').remove('agents', data)
+        return 'Delete action completed'
+
+    async def delete_operation(self, data):
+        await self.get_service('data_svc').remove('operations', data)
+        await self.get_service('data_svc').remove('sources', dict(id=str(data.get('id'))))
+        for f in glob.glob('data/results/*'):
+            if '%s-' % data.get('id') in f:
+                os.remove(f)
         return 'Delete action completed'
 
     async def display_objects(self, object_name, data):
