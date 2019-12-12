f9313292229f64cb8698e539b7412fe98fd727ff
david <49954156+privateducky@users.noreply.github.com>
2019-12-11 19:04:00 -0500

    dropping logs dir to free up open file descriptors. yes that was the … (#916)
    * dropping logs dir to free up open file descriptors. yes that was the issue
    
    * dropping logs dir to free up open file descriptors. yes that was the issue

--- app/utility/logger.py
+++ app/utility/logger.py
@@ -6,9 +6,6 @@ class Logger:
     def __init__(self, name):
         self.name = name
         self.logger = logging.getLogger(name)
-        handler = logging.FileHandler('%s/%s.log' % ('logs', name))
-        handler.setFormatter(logging.Formatter('%(asctime)s: %(message)s', '%Y-%m-%d %H:%M:%S'))
-        self.logger.addHandler(handler)
 
     def debug(self, msg):
         self.logger.debug(msg)
