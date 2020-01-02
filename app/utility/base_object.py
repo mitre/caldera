b8724d10c46d242ed3feb749eb2c0e764f42989e
Hunt, David <davidhunt@mitre.org>
2020-01-02 15:54:24 -0500

    updates

--- app/utility/base_object.py
+++ app/utility/base_object.py
@@ -4,7 +4,7 @@ from app.utility.base_world import BaseWorld
 class BaseObject(BaseWorld):
 
     RESERVED = dict(server='#{server}', group='#{group}', agent_paw='#{paw}', location='#{location}',
-                    exe_name='#{exe_name}', link_id='#{link_id}')
+                    exe_name='#{exe_name}')
 
     def match(self, criteria):
         if not criteria:
