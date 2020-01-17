b9405c936a1d86a2001f48dd682a98e9dafeb31d
david <49954156+privateducky@users.noreply.github.com>
2020-01-17 18:39:48 -0500

    updating visibility file and other data (#1058)

--- app/objects/secondclass/c_visibility.py
+++ app/objects/secondclass/c_visibility.py
@@ -8,7 +8,7 @@ Adjustment = namedtuple('Adjustment', 'trait value offset')
 
 class Visibility(BaseObject):
 
-    def __init__(self, score, adjustments=()):
+    def __init__(self, score=50, adjustments=()):
         super().__init__()
         self.score = score
         self.adjustments = [Adjustment(k, v.get('value'), v.get('offset')) for a in adjustments for k, v in a.items()]
