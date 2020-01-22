19b7409096f99738e5a0be1d3abfc2fd854df8b7
Hunt, David <davidhunt@mitre.org>
2020-01-22 14:42:26 -0500

    ensuring relationships get a default score

--- app/objects/c_relationship.py
+++ app/objects/c_relationship.py
@@ -9,14 +9,16 @@ class Relationship(BaseObject):
 
     @classmethod
     def from_json(cls, json):
-        return cls(source=json['source'], edge=json.get('edge'), target=json.get('target'))
+        return cls(source=json['source'], edge=json.get('edge'), target=json.get('target'), score=json.get('score'))
 
     @property
     def display(self):
-        return self.clean(dict(source=self.source, edge=self.edge, target=self.target))
+        return self.clean(dict(source=self.source, edge=self.edge, target=self.target, score=self.score))
 
-    def __init__(self, source, edge=None, target=None):
+    def __init__(self, source, edge=None, target=None, score=1):
         super().__init__()
         self.source = source
         self.edge = edge
         self.target = target
+        self.score = score
+
