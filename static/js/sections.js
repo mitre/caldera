278fd1b96820db934752a283ebcb7dcd0968e080
wbooth <18699738+wbooth@users.noreply.github.com>
2020-01-24 12:35:51 -0500

    plugin manager init

--- static/js/sections.js
+++ static/js/sections.js
@@ -1380,3 +1380,14 @@ function hilApproveAll(){
     refresh();
     return false;
 }
+
+$(document).ready(function() {
+    $('#plugins-table').DataTable({
+          columnDefs: [
+            {
+                targets: 0,
+                className: 'dt-body-left'
+            }
+          ]
+    })
+});
