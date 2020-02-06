6ec415bb4e070d2e62c412044cf389db357d2955
Hunt, David <davidhunt@mitre.org>
2020-02-06 16:21:16 -0700

    reverting PR

--- app/service/file_svc.py
+++ app/service/file_svc.py
@@ -1,15 +1,14 @@
-import os
 import base64
+import os
 
 from aiohttp import web
 from cryptography.fernet import Fernet
+from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
-from cryptography.hazmat.backends import default_backend
 
 from app.utility.base_service import BaseService
-from app.utility.payload_encoder import xor_file, xor_bytes
-
+from app.utility.payload_encoder import xor_file
 
 FILE_ENCRYPTION_FLAG = '%encrypted%'
 
@@ -38,9 +37,6 @@ class FileSvc(BaseService):
         if payload in self.special_payloads:
             payload, display_name = await self.special_payloads[payload](request)
         file_path, contents = await self.read_file(payload)
-        if request.headers.get('xor_key'):
-            xor_key = request.headers['xor_key']
-            contents = xor_bytes(contents, xor_key.encode())
         if request.get('name'):
             display_name = request.get('name')
         return file_path, contents, display_name
