361fc9fc16906b310d2d52743f7f9fdc6a406fb3
david <49954156+privateducky@users.noreply.github.com>
2020-02-13 18:19:37 -0500

    adding operations to blue html page (#1223)

--- app/api/packs/defensive.py
+++ app/api/packs/defensive.py
@@ -16,6 +16,7 @@ class DefensivePack(BaseWorld):
     async def enable(self):
         self.app_svc.application.router.add_route('GET', '/defense/agents', self._section_agent)
         self.app_svc.application.router.add_route('GET', '/defense/profiles', self._section_profiles)
+        self.app_svc.application.router.add_route('GET', '/defense/operations', self._section_operations)
 
     """ PRIVATE """
 
@@ -34,3 +35,16 @@ class DefensivePack(BaseWorld):
         adversaries = [a.display for a in await self.data_svc.locate('adversaries', match=self._search)]
         return dict(adversaries=adversaries, exploits=[a.display for a in abilities], payloads=payloads,
                     tactics=tactics)
+
+    @blue_authorization
+    @template('operations.html')
+    async def _section_operations(self, request):
+        hosts = [h.display for h in await self.data_svc.locate('agents', match=self._search)]
+        groups = list(set(([h['group'] for h in hosts])))
+        adversaries = [a.display for a in await self.data_svc.locate('adversaries', match=self._search)]
+        sources = [s.display for s in await self.data_svc.locate('sources', match=self._search)]
+        planners = [p.display for p in await self.data_svc.locate('planners')]
+        obfuscators = [o.display for o in await self.data_svc.locate('obfuscators')]
+        operations = [o.display for o in await self.data_svc.locate('operations')]
+        return dict(operations=operations, groups=groups, adversaries=adversaries, sources=sources, planners=planners,
+                    obfuscators=obfuscators)
