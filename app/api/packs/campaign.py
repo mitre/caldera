412e1e6f2c83a34f44347cd9b06df4c23073ad2d
Hunt, David <davidhunt@mitre.org>
2020-02-14 09:43:59 -0500

    simplifying api endpoints, again

--- app/api/packs/campaign.py
+++ app/api/packs/campaign.py
@@ -1,48 +1,50 @@
 from aiohttp_jinja2 import template
 
-from app.service.auth_svc import blue_authorization
+from app.service.auth_svc import check_authorization
 from app.utility.base_world import BaseWorld
 
 
-class DefensivePack(BaseWorld):
+class CampaignPack(BaseWorld):
 
     def __init__(self, services):
-        self._search = dict(access=(self.Access.BLUE, self.Access.APP))
         self.auth_svc = services.get('auth_svc')
         self.app_svc = services.get('app_svc')
         self.data_svc = services.get('data_svc')
         self.rest_svc = services.get('rest_svc')
 
     async def enable(self):
-        self.app_svc.application.router.add_route('GET', '/defense/agents', self._section_agent)
-        self.app_svc.application.router.add_route('GET', '/defense/profiles', self._section_profiles)
-        self.app_svc.application.router.add_route('GET', '/defense/operations', self._section_operations)
+        self.app_svc.application.router.add_route('GET', '/campaign/agents', self._section_agent)
+        self.app_svc.application.router.add_route('GET', '/campaign/profiles', self._section_profiles)
+        self.app_svc.application.router.add_route('GET', '/campaign/operations', self._section_operations)
 
     """ PRIVATE """
 
-    @blue_authorization
+    @check_authorization
     @template('agents.html')
     async def _section_agent(self, request):
-        agents = [h.display for h in await self.data_svc.locate('agents', match=self._search)]
+        access = dict(access=(await self.auth_svc.get_permissions(request)))
+        agents = [h.display for h in await self.data_svc.locate('agents', match=access)]
         return dict(agents=agents)
 
-    @blue_authorization
+    @check_authorization
     @template('profiles.html')
     async def _section_profiles(self, request):
-        abilities = await self.data_svc.locate('abilities', match=self._search)
+        access = dict(access=(await self.auth_svc.get_permissions(request)))
+        abilities = await self.data_svc.locate('abilities',match=access)
         tactics = set([a.tactic.lower() for a in abilities])
         payloads = await self.rest_svc.list_payloads()
-        adversaries = [a.display for a in await self.data_svc.locate('adversaries', match=self._search)]
+        adversaries = [a.display for a in await self.data_svc.locate('adversaries', match=access)]
         return dict(adversaries=adversaries, exploits=[a.display for a in abilities], payloads=payloads,
                     tactics=tactics)
 
-    @blue_authorization
+    @check_authorization
     @template('operations.html')
     async def _section_operations(self, request):
-        hosts = [h.display for h in await self.data_svc.locate('agents', match=self._search)]
+        access = dict(access=(await self.auth_svc.get_permissions(request)))
+        hosts = [h.display for h in await self.data_svc.locate('agents', match=access)]
         groups = list(set(([h['group'] for h in hosts])))
-        adversaries = [a.display for a in await self.data_svc.locate('adversaries', match=self._search)]
-        sources = [s.display for s in await self.data_svc.locate('sources', match=self._search)]
+        adversaries = [a.display for a in await self.data_svc.locate('adversaries', match=access)]
+        sources = [s.display for s in await self.data_svc.locate('sources', match=access)]
         planners = [p.display for p in await self.data_svc.locate('planners')]
         obfuscators = [o.display for o in await self.data_svc.locate('obfuscators')]
         operations = [o.display for o in await self.data_svc.locate('operations')]
