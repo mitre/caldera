4255e858a348bdf3124ab1b4d44da0de86a1cdf6
javon <javon@mitre.org>
2020-02-17 12:07:21 -0500

    Deleted test plugins

--- plugins/pluginA/hook.py
+++
@@ -1,9 +0,0 @@
-from app.utility.base_world import BaseWorld
-
-name = 'PluginA'
-description = 'Test plugin'
-address = None
-access = BaseWorld.Access.RED
-
-async def enable(services):
-    x = 2+2
