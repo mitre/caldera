4bbe5c088334240b35ff0ffff35b994bf31465e1
Hunt, David <davidhunt@mitre.org>
2020-02-17 17:10:12 -0500

    updates

--- app/contacts/handles/h_chat.py
+++ app/contacts/handles/h_chat.py
@@ -1,6 +1,3 @@
-import websockets
-
-
 class Handle:
 
     def __init__(self, tag):
@@ -8,10 +5,6 @@ class Handle:
 
     @staticmethod
     async def run(socket, path, services):
-        message = await socket.recv()
-        print(message)
-        await socket.send(message)
-        #uri = 'ws://127.0.0.1:7012/chat'
-        #async with websockets.connect(uri) as websocket:
-        #    print('forwarding to UI...')
-        #    await websocket.send(message)
+        while True:
+            message = await socket.recv()
+            await socket.send(message)
