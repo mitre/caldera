59611d372a3df0eacdb32dd9d8433e96ee85bc2e
mchan143 <61020544+mchan143@users.noreply.github.com>
2020-02-18 17:50:12 -0500

    bug fixes for saving sources and regex for file paths (#1262)
    * bug fix for saving sources
    
    * bug fix for regex processing of directory paths

--- app/utility/base_planning_svc.py
+++ app/utility/base_planning_svc.py
@@ -121,7 +121,7 @@ class BasePlanningService(BaseService):
             score += (score + var.score)
             used.append(var)
             re_variable = re.compile(r'#{(%s.*?)}' % var.trait, flags=re.DOTALL)
-            copy_test = re.sub(re_variable, str(var.value).strip(), copy_test)
+            copy_test = re.sub(re_variable, str(var.value).strip().encode('unicode-escape').decode('utf-8'), copy_test)
         return copy_test, score, used
 
     @staticmethod
