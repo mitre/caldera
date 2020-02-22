157e50b06ca3312b844917d4927c59600aa0b682
Hunt, David <davidhunt@mitre.org>
2020-02-22 14:15:31 -0500

    organizing tests into directories

---
+++ tests/base/test_base.py
@@ -0,0 +1,21 @@
+import asyncio
+import unittest
+
+from app.service.app_svc import AppService
+from app.service.data_svc import DataService
+from app.service.planning_svc import PlanningService
+from app.service.rest_svc import RestService
+
+
+class TestBase(unittest.TestCase):
+
+    def initialize(self):
+        self.app_svc = AppService(None)
+        self.data_svc = DataService()
+        self.rest_svc = RestService()
+        self.planning_svc = PlanningService()
+        self.services = [self.app_svc.get_services()]
+
+    @staticmethod
+    def run_async(c):
+        return asyncio.get_event_loop().run_until_complete(c)
--- tests/objects/test_fact.py
+++ tests/objects/test_fact.py
@@ -4,6 +4,7 @@ from app.objects.secondclass.c_fact import Fact
 
 
 class TestFact(unittest.TestCase):
+
     def test_escaped_cmd(self):
         test_fact = Fact('test', 'test value| &')
         self.assertEqual(test_fact.escaped('cmd'), 'test^ value^|^ ^&')
