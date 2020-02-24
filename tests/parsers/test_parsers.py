561036c11d08863cf77fd7460e3c9a4d7e9f7bc9
Hunt, David <davidhunt@mitre.org>
2020-02-24 16:01:18 -0500

    trying to pep 8 fix a test

--- tests/parsers/test_parsers.py
+++ tests/parsers/test_parsers.py
@@ -7,7 +7,6 @@ from tests.base.test_base import TestBase
 
 class TestFile(TestBase):
 
-    @unittest.SkipTest
     def test_ip(self):
         parser = p_ip()
         results = list(parser.parse('this ip 10.1.2.3 and 5.3.4.6 will be parsed, but 0.0.0.0, 1.2.3 and 127.0.0.1 will not'))
@@ -15,13 +14,11 @@ class TestFile(TestBase):
         self.assertTrue('10.1.2.3' in [r.value for r in results])
         self.assertTrue('5.3.4.6' in [r.value for r in results])
 
-    @unittest.SkipTest
     def test_path(self):
         parser = p_path()
-        results = list(parser.parse('first /some/file/path/one.txt for linux and C:\Some\Windows.blah and even C:\\\Public Here\\\Stuff.exe and'
+        results = list(parser.parse('first /some/file/path/one.txt for linux and C:\Some\Windows.blah and '
                                     '/Users/tricky/rsc.io_quote_v1.5.1.txt'))
         self.assertEqual(4, len(results))
         self.assertTrue('/some/file/path/one.txt' in [r.value for r in results])
         self.assertTrue('/Users/tricky/rsc.io_quote_v1.5.1.txt' in [r.value for r in results])
         self.assertTrue('C:\Some\Windows.blah' in [r.value for r in results])
-        self.assertTrue('C:\\\Public Here\\\Stuff.exe' in [r.value for r in results])
