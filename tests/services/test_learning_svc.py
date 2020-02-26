8c8f1ad9b55b0e96bff5406c92e62e1e38f3a6e9
Hunt, David <davidhunt@mitre.org>
2020-02-26 11:11:42 -0500

    adding dynamic relationship building to the learning service

--- tests/services/test_learning_svc.py
+++ tests/services/test_learning_svc.py
@@ -1,5 +1,6 @@
 from app.objects.c_ability import Ability
 from app.objects.c_operation import Operation
+from app.objects.secondclass.c_fact import Fact
 from app.objects.secondclass.c_link import Link
 from app.utility.base_world import BaseWorld
 from tests.base.test_base import TestBase
@@ -25,3 +26,17 @@ class TestLearningSvc(TestBase):
             blob=BaseWorld.encode_string('i contain 1 ip address 192.168.0.1 and one file /etc/host.txt. that is all.'))
         )
         self.assertEqual(2, len(link.facts))
+
+    def test_build_relationships(self):
+        self.learning_svc.model.add(frozenset({'host.user.name', 'target.org.name'}))
+        self.learning_svc.model.add(frozenset({'host.file.extension', 'host.user.name', 'domain.user.name'}))
+        facts = [
+            Fact(trait='target.org.name', value='something'),
+            Fact(trait='host.user.name', value='admin'),
+            Fact(trait='host.user.name', value='root'),
+            Fact(trait='domain.user.name', value='user'),
+            Fact(trait='not.really.here', value='should never be found')
+        ]
+        link = Link(operation=self.operation.id, ability=self.ability, command=None, paw=None)
+        self.run_async(self.learning_svc._build_relationships(link, facts))
+        self.assertEqual(4, len(link.relationships))
