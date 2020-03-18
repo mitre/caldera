12cf48f8d8a47679f7f48840343842bd05f3db98
Henry Foster <hfoster@mitre.org>
2020-03-18 16:05:22 -0400

    use default config and load plugins at initialization

--- tests/base/test_base.py
+++ tests/base/test_base.py
@@ -8,7 +8,9 @@ from aiohttp.test_utils import AioHTTPTestCase
 from app.api.rest_api import RestApi
 from app.service.app_svc import AppService
 from app.service.auth_svc import AuthService
+from app.service.contact_svc import ContactService
 from app.service.data_svc import DataService
+from app.service.file_svc import FileSvc
 from app.service.learning_svc import LearningService
 from app.service.planning_svc import PlanningService
 from app.service.rest_svc import RestService
@@ -16,39 +18,23 @@ from app.utility.base_world import BaseWorld
 
 
 class TestBase(AioHTTPTestCase):
-    test_config = """
-        api_key: ADMIN123
-        app.contact.html: /weather
-        app.contact.http: http://127.0.0.1:8888
-        app.contact.tcp: 127.0.0.1:7010
-        app.contact.udp: 127.0.0.1:7011
-        app.contact.websocket: 127.0.0.1:7012
-        crypt_salt: REPLACE_WITH_RANDOM_VALUE
-        exfil_dir: /tmp
-        plugins: []
-        port: 8888
-        reports_dir: /tmp
-        users:
-          blue:
-            blue: admin
-          red:
-            admin: admin
-            red: admin
-        """
 
     @classmethod
     def patch_config(cls):
-        BaseWorld.apply_config('default', yaml.safe_load(cls.test_config))
+        with open(Path(__file__).parents[2] / 'conf' / 'default.yml', 'r') as fle:
+            BaseWorld.apply_config('default', yaml.safe_load(fle))
 
     def initialize(self):
+        self.patch_config()
         self.app_svc = AppService(web.Application())
         self.data_svc = DataService()
         self.rest_svc = RestService()
         self.planning_svc = PlanningService()
         self.learning_svc = LearningService()
         self.auth_svc = AuthService()
+        self.contact_svc = ContactService()
+        self.file_svc = FileSvc()
         self.services = self.app_svc.get_services()
-        self.patch_config()
 
     async def get_application(self):
         """
@@ -56,6 +42,7 @@ class TestBase(AioHTTPTestCase):
         """
         os.chdir(str(Path(__file__).parents[2]))
         self.initialize()
+        await self.app_svc.register_contacts()
         await self.app_svc.load_plugins()
         self.rest_api = await RestApi(self.services).enable()
         await self.auth_svc.apply(self.app_svc.application, self.auth_svc.get_config('users'))
