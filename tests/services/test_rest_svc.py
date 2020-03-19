07aec7270fc6759f4173418868f03d01a0d10ad3
Tran, Chris <ctran@mitre.org>
2020-03-19 15:35:25 -0400

    update test_rest_svc to utilize pytest fixtures and assertions

--- tests/services/test_rest_svc.py
+++ tests/services/test_rest_svc.py
@@ -1,33 +1,37 @@
+import pytest
+
 from app.objects.c_ability import Ability
 from app.utility.base_world import BaseWorld
-from tests.base.test_base import TestBase
 
 
-class TestRestSvc(TestBase):
+@pytest.fixture
+def setup_rest_svc_test(loop, data_svc):
+    BaseWorld.apply_config(name='default', config={'app.contact.http': '0.0.0.0', 'plugins': ['sandcat', 'stockpile']})
+    loop.run_until_complete(data_svc.store(
+        Ability(ability_id='123', test=BaseWorld.encode_string('curl #{app.contact.http}'), variations=[]))
+    )
 
-    def setUp(self):
-        self.initialize()
-        BaseWorld.apply_config(name='default', config={'app.contact.http': '0.0.0.0', 'plugins': ['sandcat', 'stockpile']})
-        self.run_async(self.data_svc.store(
-            Ability(ability_id='123', test=BaseWorld.encode_string('curl #{app.contact.http}'), variations=[]))
-        )
 
-    def test_update_config(self):
+class TestRestSvc:
+
+    def test_update_config(self, loop, setup_rest_svc_test, data_svc, rest_svc):
+        internal_rest_svc = rest_svc(loop)
         # check that an ability reflects the value in app. property
-        pre_ability = self.run_async(self.data_svc.locate('abilities', dict(ability_id='123')))
-        self.assertEqual('0.0.0.0', BaseWorld.get_config('app.contact.http'))
-        self.assertEqual('curl 0.0.0.0', BaseWorld.decode_bytes(pre_ability[0].test))
+        pre_ability = loop.run_until_complete(data_svc.locate('abilities', dict(ability_id='123')))
+        assert '0.0.0.0' == BaseWorld.get_config('app.contact.http')
+        assert 'curl 0.0.0.0' == BaseWorld.decode_bytes(pre_ability[0].test)
 
         # update property
-        self.run_async(self.rest_svc.update_config(data=dict(prop='app.contact.http', value='127.0.0.1')))
+        loop.run_until_complete(internal_rest_svc.update_config(data=dict(prop='app.contact.http', value='127.0.0.1')))
 
         # verify ability reflects new value
-        post_ability = self.run_async(self.data_svc.locate('abilities', dict(ability_id='123')))
-        self.assertEqual('127.0.0.1', BaseWorld.get_config('app.contact.http'))
-        self.assertEqual('curl 127.0.0.1', BaseWorld.decode_bytes(post_ability[0].test))
+        post_ability = loop.run_until_complete(data_svc.locate('abilities', dict(ability_id='123')))
+        assert '127.0.0.1' == BaseWorld.get_config('app.contact.http')
+        assert 'curl 127.0.0.1' == BaseWorld.decode_bytes(post_ability[0].test)
 
-    def test_update_config_plugin(self):
+    def test_update_config_plugin(self, loop, setup_rest_svc_test, rest_svc):
+        internal_rest_svc = rest_svc(loop)
         # update plugin property
-        self.assertEqual(['sandcat', 'stockpile'], BaseWorld.get_config('plugins'))
-        self.run_async(self.rest_svc.update_config(data=dict(prop='plugin', value='ssl')))
-        self.assertEqual(['sandcat', 'stockpile', 'ssl'], BaseWorld.get_config('plugins'))
+        assert ['sandcat', 'stockpile'] == BaseWorld.get_config('plugins')
+        loop.run_until_complete(internal_rest_svc.update_config(data=dict(prop='plugin', value='ssl')))
+        assert ['sandcat', 'stockpile', 'ssl'] == BaseWorld.get_config('plugins')
