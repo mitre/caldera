eff9d7ddeeb56816ce06d1ae657398a3a083d5d6
amanners <amanners@mitre.org>
2020-03-20 21:17:06 -0400

    adding create operation test

--- tests/services/test_rest_svc.py
+++ tests/services/test_rest_svc.py
@@ -3,6 +3,8 @@ import pytest
 from app.objects.c_ability import Ability
 from app.objects.c_agent import Agent
 from app.objects.c_adversary import Adversary
+from app.objects.c_planner import Planner
+from app.objects.c_source import Source
 from app.utility.base_world import BaseWorld
 
 
@@ -14,13 +16,19 @@ def setup_rest_svc_test(loop, data_svc):
                                                    'api_key': 'ADMIN123',
                                                    'exfil_dir': '/tmp'})
     loop.run_until_complete(data_svc.store(
-        Ability(ability_id='123', test=BaseWorld.encode_string('curl #{app.contact.http}'), variations=[]))
-    )
+        Ability(ability_id='123', test=BaseWorld.encode_string('curl #{app.contact.http}'), variations=[])
+    ))
     loop.run_until_complete(data_svc.store(
-        Adversary(adversary_id='123', name='test', description='test', phases=[]))
-    )
+        Adversary(adversary_id='123', name='test', description='test', phases=dict())
+    ))
     loop.run_until_complete(data_svc.store(
         Agent(paw='123', sleep_min=2, sleep_max=8, watchdog=0)
+    ))
+    loop.run_until_complete(data_svc.store(
+        Planner(planner_id='123', name='test', module='test', params=dict())
+    ))
+    loop.run_until_complete(data_svc.store(
+        Source(identifier='123', name='test', facts=[])
     ))
 
 
@@ -50,6 +58,25 @@ class TestRestSvc:
         assert ['sandcat', 'stockpile'] == BaseWorld.get_config('plugins')
         loop.run_until_complete(internal_rest_svc.update_config(data=dict(prop='plugin', value='ssl')))
         assert ['sandcat', 'stockpile', 'ssl'] == BaseWorld.get_config('plugins')
+
+    def test_create_operation(self, loop, rest_svc, data_svc):
+        want = {'name': 'Test',
+                'host_group': [{'paw': '123', 'group': 'red', 'architecture': 'unknown', 'platform': 'unknown',
+                                'server': '://None:None', 'location': 'unknown', 'pid': 0, 'ppid': 0, 'trusted': True,
+                                'sleep_min': 2, 'sleep_max': 8, 'executors': (), 'privilege': 'User',
+                                'display_name': 'unknown$unknown', 'exe_name': 'unknown', 'host': 'unknown',
+                                'watchdog': 0, 'contact': 'unknown'}],
+                'adversary': {'adversary_id': '123', 'name': 'test', 'description': 'test', 'phases': {}},
+                'jitter': '2/8', 'source': '', 'planner': 'test', 'state': 'finished', 'phase': 0,
+                'obfuscator': 'plain-text', 'autonomous': 1, 'finish': '', 'chain': []}
+        internal_rest_svc = rest_svc(loop)
+        operation = loop.run_until_complete(internal_rest_svc.create_operation(access=dict(
+            access=(internal_rest_svc.Access.RED, internal_rest_svc.Access.APP)),
+            data=dict(name='Test', planner='test', adversary_id='123', source='123', state='finished')))
+        operation[0].pop('id')
+        operation[0]['host_group'][0].pop('last_seen')
+        operation[0].pop('start')
+        assert want == operation[0]
 
     def test_delete_ability(self, loop, rest_svc, file_svc):
         internal_rest_svc = rest_svc(loop)
