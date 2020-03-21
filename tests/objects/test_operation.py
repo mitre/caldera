9db2060f7d828f9148bd35d2c44135880f1f9686
Tran, Chris <ctran@mitre.org>
2020-03-21 18:26:31 -0400

    add tests for payload obfuscation

--- tests/objects/test_operation.py
+++ tests/objects/test_operation.py
@@ -22,15 +22,37 @@ class TestOperation:
         toperation = operation(name='my first op', agents=[], adversary=adversary)
         yield toperation
 
-    def test__get_unique_payload_key_no_real_payloads(self, loop, demo_operation):
+    def test__get_unique_payload_key_no_real_payloads(self, demo_operation):
         payloads = BaseWorld.get_config(prop='obfuscated_payloads', name='abilities').get('names')
         payload_name = demo_operation._get_unique_payload_key(payload_name_len=10)
         assert payload_name in payloads
 
-    def test__get_unique_payload_key_force_random(self, loop, demo_operation):
+    def test__get_unique_payload_key_force_random(self, demo_operation):
         payloads = BaseWorld.get_config(prop='obfuscated_payloads', name='abilities').get('names')
         for name in payloads:
             demo_operation.payloads_map['to_real_payload'][name] = ''
         payload_name = demo_operation._get_unique_payload_key(payload_name_len=10)
         assert payload_name not in payloads
         assert len(payload_name) == 10
+
+    def test_apply_payload_obfuscation(self, loop, ability, demo_operation):
+        orig_payload_name = 'wifi.sh'
+        decoded_command = './%s scan' % orig_payload_name
+
+        tability = ability(payload=orig_payload_name)
+        payload_name = loop.run_until_complete(demo_operation.apply_payload_obfuscation(decoded_command, tability))
+        print(payload_name)
+        assert orig_payload_name not in payload_name
+
+    def test_apply_payload_obfuscation_predetermined_mapping(self, loop, ability, demo_operation):
+        orig_payload_name = 'predeterminedobfuscation.exe'
+        decoded_command = './%s arg' % orig_payload_name
+        mapped_payload_name = 'unittestobfuscation'
+
+        # make mapping in payload map
+        demo_operation.payloads_map['to_obfuscated_payload'][orig_payload_name] = mapped_payload_name
+
+        tability = ability(payload=orig_payload_name)
+        payload_name = loop.run_until_complete(demo_operation.apply_payload_obfuscation(decoded_command, tability))
+        print(payload_name)
+        assert mapped_payload_name in payload_name
