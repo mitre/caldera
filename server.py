500b76d312e448aea46d079af64e5e9af5186a24
clittle <clittle@mitre.org>
2020-03-27 16:04:01 -0400

    Fixed a bug where custom adv would load too early, before their abilities would be loaded from plugins. Also made some changes to how the web interface handles packs.

--- server.py
+++ server.py
@@ -50,6 +50,7 @@ def run_tasks(services):
     loop.run_until_complete(app_svc.register_contacts())
     loop.run_until_complete(app_svc.load_plugins())
     loop.run_until_complete(data_svc.load_data())
+    loop.run_until_complete(data_svc.load_data([Plugin(data_dir='data')]))
     loop.create_task(app_svc.start_sniffer_untrusted_agents())
     loop.create_task(app_svc.resume_operations())
     loop.create_task(app_svc.run_scheduler())
