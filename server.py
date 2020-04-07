98c88bd664748247b262d12c5ac7fb2a772b7a17
clittle <clittle@mitre.org>
2020-04-07 10:39:48 -0400

    Full rebase

--- server.py
+++ server.py
@@ -49,7 +49,6 @@ def run_tasks(services):
     loop.run_until_complete(RestApi(services).enable())
     loop.run_until_complete(app_svc.register_contacts())
     loop.run_until_complete(app_svc.load_plugins())
-    loop.run_until_complete(data_svc.load_data([Plugin(data_dir='data')]))
     loop.run_until_complete(data_svc.load_data())
     loop.create_task(app_svc.start_sniffer_untrusted_agents())
     loop.create_task(app_svc.resume_operations())
