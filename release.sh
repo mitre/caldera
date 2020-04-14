15b21d2791d7aacaca00937496fc9f1d295079ee
privateducky <davidhunt@mitre.org>
2020-04-14 16:32:56 -0400

    update

--- release.sh
+++ release.sh
@@ -8,7 +8,7 @@ function tag_version(){
 
 function update_version(){
     git reset --hard origin/master && git checkout master && git pull
-    newHash=$(dirhash . -a md5 -m "*.py")
+    newHash=$(dirhash . -a md5 -i "/plugins/ /.tox/" -m "*.py *.html")
     echo "${1}-${newHash}" > VERSION.txt
     tag_version $1
 }
