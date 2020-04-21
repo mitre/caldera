b72efbaee3554038047728757fd2f024860b5361
amanners <amanners@mitre.org>
2020-04-21 15:31:30 -0400

    fixing whitespace

--- app/service/file_svc.py
+++ app/service/file_svc.py
@@ -167,7 +167,7 @@ class FileSvc(FileServiceInterface, BaseService):
     async def _operate_extension(self, payload, headers):
         try:
             target = '.' + payload.split('.')[-1]
-            return await self.special_payloads[target](self.get_services(),headers)
+            return await self.special_payloads[target](self.get_services(), headers)
         except Exception as e:
             self.log.error('Error loading extension handler=%s, %s' % (payload, e))
 
