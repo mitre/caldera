9d81132c732c9ed715f1a074fbf6008ab971cfe6
mchan143 <61020544+mchan143@users.noreply.github.com>
2020-04-27 12:27:38 -0400

    tests for agent, rest_svc, planning_svc

--- tests/services/test_planning_svc.py
+++ tests/services/test_planning_svc.py
@@ -1,16 +1,27 @@
 import pytest
 
+from app.objects.c_obfuscator import Obfuscator
+from app.objects.c_source import Source
 from app.objects.secondclass.c_link import Link
 from app.utility.base_world import BaseWorld
 
 
 @pytest.fixture
-def setup_planning_test(loop, ability, agent, operation, data_svc):
-    tability = ability(ability_id='123', executor='sh', test=BaseWorld.encode_string('mkdir test'),
+def setup_planning_test(loop, ability, agent, operation, data_svc, init_base_world):
+    tability = ability(ability_id='123', executor='sh', platform='darwin', test=BaseWorld.encode_string('mkdir test'),
                        cleanup=BaseWorld.encode_string('rm -rf test'), variations=[])
-    tagent = agent(sleep_min=1, sleep_max=2, watchdog=0)
-    toperation = operation(name='test1', agents=tagent, adversary='hunter')
+    tagent = agent(sleep_min=1, sleep_max=2, watchdog=0, executors=['sh'], platform='darwin')
+    tsource = Source(identifier='123', name='test', facts=[], adjustments=[])
+    toperation = operation(name='test1', agents=tagent, adversary='hunter', source=tsource)
+
     loop.run_until_complete(data_svc.store(tability))
+
+    loop.run_until_complete(data_svc.store(
+        Obfuscator(name='plain-text',
+                   description='Does no obfuscation to any command, instead running it in plain text',
+                   module='plugins.stockpile.app.obfuscators.plain_text')
+    ))
+
     yield (tability, tagent, toperation)
 
 
@@ -25,3 +36,8 @@ class TestPlanningService:
         link_list = list(links)
         assert len(link_list) == 1
         assert link_list[0].command == ability.cleanup[0]
+
+    def test_generate_and_trim_links(self, loop, setup_planning_test, planning_svc):
+        ability, agent, operation = setup_planning_test
+        generated_links = loop.run_until_complete(planning_svc.generate_and_trim_links(agent, operation, [ability]))
+        assert 1 == len(generated_links)
