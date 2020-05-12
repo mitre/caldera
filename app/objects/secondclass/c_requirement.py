4453f0d058d6a65901d2d584479f7b35b8c3bf7c
mchan143 <61020544+mchan143@users.noreply.github.com>
2020-05-12 08:48:35 -0400

    initial schema for requirement

--- app/objects/secondclass/c_requirement.py
+++ app/objects/secondclass/c_requirement.py
@@ -1,8 +1,22 @@
+import marshmallow as ma
+
 from app.objects.secondclass.c_relationship import Relationship
 from app.utility.base_object import BaseObject
 
 
+class RequirementSchema(ma.Schema):
+
+    module = ma.fields.String()
+    relationships = ma.fields.Function(lambda obj: [r.display for r in obj.relationships]) # temp - replace with Nested(RelationshipSchema)
+
+    @ma.post_load()
+    def build_source(self, data, **_):
+        return Requirement(**data)
+
+
 class Requirement(BaseObject):
+
+    schema = RequirementSchema()
 
     @property
     def unique(self):
@@ -12,10 +26,6 @@ class Requirement(BaseObject):
     def from_json(cls, json):
         relationships = [Relationship.from_json(r) for r in json['relationships']]
         return cls(module=json['module'], relationships=relationships)
-
-    @property
-    def display(self):
-        return dict(module=self.module, relationships=[r.display for r in self.relationships])
 
     def __init__(self, module, relationships):
         super().__init__()
