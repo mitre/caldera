a70c4f8b013a0d14766a9674577276e67cd419b1
wbooth <18699738+wbooth@users.noreply.github.com>
2020-05-13 07:56:26 -0400

    Schedule Schema (#1643)

--- app/objects/c_schedule.py
+++ app/objects/c_schedule.py
@@ -1,16 +1,22 @@
+import marshmallow as ma
+
 from app.objects.interfaces.i_object import FirstClassObjectInterface
 from app.utility.base_object import BaseObject
 
 
+class ScheduleSchema(ma.Schema):
+
+    name = ma.fields.String()
+    schedule = ma.fields.Time()
+    task = ma.fields.Function(lambda obj: obj.task.display)
+
+
 class Schedule(FirstClassObjectInterface, BaseObject):
+    schema = ScheduleSchema()
 
     @property
     def unique(self):
         return self.hash('%s' % self.name)
-
-    @property
-    def display(self):
-        return dict()
 
     def __init__(self, name, schedule, task):
         super().__init__()
