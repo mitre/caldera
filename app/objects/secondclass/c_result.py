967e1056062f8901583a551171842c3e9ede5cfa
Fred <59877971+mrengstrom@users.noreply.github.com>
2020-05-18 05:58:14 -0500

    adding result schema (#1663)

--- app/objects/secondclass/c_result.py
+++ app/objects/secondclass/c_result.py
@@ -1,7 +1,22 @@
+import marshmallow as ma
+
 from app.utility.base_object import BaseObject
 
 
+class ResultSchema(ma.Schema):
+    id = ma.fields.String()
+    output = ma.fields.String()
+    pid = ma.fields.String()
+    status = ma.fields.String()
+
+    @ma.post_load
+    def build_result(self, data, **_):
+        return Result(**data)
+
+
 class Result(BaseObject):
+
+    schema = ResultSchema()
 
     def __init__(self, id, output, pid=0, status=0):
         super().__init__()
