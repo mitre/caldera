f1089769c45516da11ec70e90d13ef45352f44dd
privateducky <davidhunt@mitre.org>
2020-05-20 09:53:49 -0400

    ensuring tasking agents can utilize obfuscation

--- tests/objects/test_agent.py
+++ tests/objects/test_agent.py
@@ -10,14 +10,14 @@ class TestAgent:
         ability = Ability(ability_id='123', test=BaseWorld.encode_string('whoami'), variations=[],
                           executor='psh', platform='windows')
         agent = Agent(paw='123', sleep_min=2, sleep_max=8, watchdog=0, executors=['pwsh', 'psh'], platform='windows')
-        loop.run_until_complete(agent.task([ability]))
+        loop.run_until_complete(agent.task([ability], obfuscator='plain-text'))
         assert 1 == len(agent.links)
 
     def test_task_missing_fact(self, loop, init_base_world):
         ability = Ability(ability_id='123', test=BaseWorld.encode_string('net user #{domain.user.name} /domain'),
                           variations=[], executor='psh', platform='windows')
         agent = Agent(paw='123', sleep_min=2, sleep_max=8, watchdog=0, executors=['pwsh', 'psh'], platform='windows')
-        loop.run_until_complete(agent.task([ability]))
+        loop.run_until_complete(agent.task([ability], obfuscator='plain-text'))
         assert 0 == len(agent.links)
 
     def test_task_with_facts(self, loop, init_base_world):
@@ -26,5 +26,5 @@ class TestAgent:
         agent = Agent(paw='123', sleep_min=2, sleep_max=8, watchdog=0, executors=['pwsh', 'psh'], platform='windows')
         fact = Fact(trait='domain.user.name', value='bob')
 
-        loop.run_until_complete(agent.task([ability], [fact]))
+        loop.run_until_complete(agent.task([ability], 'plain-text', [fact]))
         assert 1 == len(agent.links)
