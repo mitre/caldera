b5253b5fea77b20fbb7ac36c72ad7da87317c3a5
Govardhen Arunagiri <garunagiri@mitre.org>
2020-06-05 11:08:16 -0400

    default val is not mutable, and moved loading of additional_info to separate function

--- app/objects/c_ability.py
+++ app/objects/c_ability.py
@@ -66,7 +66,7 @@ class Ability(FirstClassObjectInterface, BaseObject):
     def __init__(self, ability_id, tactic=None, technique_id=None, technique=None, name=None, test=None,
                  description=None, cleanup=None, executor=None, platform=None, payloads=None, parsers=None,
                  requirements=None, privilege=None, timeout=60, repeatable=False, buckets=None, access=None,
-                 variations=None, language=None, code=None, build_target=None, additional_info=dict(), **kwargs):
+                 variations=None, language=None, code=None, build_target=None, additional_info=None, **kwargs):
         super().__init__()
         self._test = test
         self.ability_id = ability_id
@@ -91,7 +91,10 @@ class Ability(FirstClassObjectInterface, BaseObject):
         self.buckets = buckets if buckets else []
         if access:
             self.access = self.Access(access)
-        self.additional_info = additional_info
+        self.additional_info = additional_info or dict()
+        self.load_additional_info(**kwargs)
+
+    def load_additional_info(self, **kwargs):
         for k, v in kwargs.items():
             self.additional_info[k] = v
 
