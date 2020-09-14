4baac12d8bcf554bea6a2a15664f6b703a223771
mchan143 <61020544+mchan143@users.noreply.github.com>
2020-09-14 14:12:51 -0400

    config tab and configurable C2 address in agent cmd

--- app/api/rest_api.py
+++ app/api/rest_api.py
@@ -97,7 +97,8 @@ class RestApi(BaseWorld):
                     configuration=lambda d: self.rest_svc.update_config(d),
                     link=lambda d: self.rest_svc.get_potential_links(**d),
                     operation=lambda d: self.rest_svc.update_operation(**d),
-                    task=lambda d: self.rest_svc.task_agent_with_ability(**d)
+                    task=lambda d: self.rest_svc.task_agent_with_ability(**d),
+                    agent_configuration=lambda d: self.rest_svc.get_agent_configuration(d)
                 )
             )
             if index not in options[request.method]:
