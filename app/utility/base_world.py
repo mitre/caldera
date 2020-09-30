6ce88a15b07c14778e9afb1de24be55ca0b8337a
Chris Lenk <clenk@mitre.org>
2020-09-30 11:17:34 -0400

    Check if metasploit is installed

--- app/utility/base_world.py
+++ app/utility/base_world.py
@@ -122,7 +122,7 @@ class BaseWorld:
             return compare_versions(mod_version, version)
 
         def check_program_version(command, version, **kwargs):
-            output = subprocess.check_output(command.split(' '), shell=False)
+            output = subprocess.check_output(command.split(' '), stderr=subprocess.STDOUT, shell=False)
             return compare_versions(output.decode('utf-8'), version)
 
         def compare_versions(version_string, minimum_version):
