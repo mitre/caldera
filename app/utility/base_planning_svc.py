f7d11c273dfb02d049352e18d9c2b4e23a95f9c9
clittle <clittle@mitre.org>
2021-01-15 16:54:06 -0500

    Fix oversight of filtering out gray agent links by accident

--- app/utility/base_planning_svc.py
+++ app/utility/base_planning_svc.py
@@ -49,7 +49,8 @@ class BasePlanningService(BaseService):
             decoded_test = agent.replace(link.command, file_svc=self.get_service('file_svc'))
             variables = re.findall(self.re_variable, decoded_test)
             if variables:
-                relevant_facts = await self._build_relevant_facts([x for x in variables if '_' not in x], facts)
+                relevant_facts = await self._build_relevant_facts([x for x in variables if len(x.split('.')) > 2],
+                                                                  facts)
                 if all(relevant_facts):
                     good_facts = [await RuleSet(rules=rules).apply_rules(facts=fact_set) for fact_set in relevant_facts]
                     valid_facts = [await self._trim_by_limit(decoded_test, g_fact[0]) for g_fact in good_facts]
