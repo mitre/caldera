4d57e0c892c631bbfa63a2f23faf36025742d479
Caleb Little <50213210+ArtificialErmine@users.noreply.github.com>
2021-01-22 17:12:46 -0500

    limit filtering fix (#2014)
    * Patches to allow limit filtering to work properly
    
    * Simplification
    
    Quick cleanup of initial copy

--- app/utility/base_planning_svc.py
+++ app/utility/base_planning_svc.py
@@ -198,14 +198,15 @@ class BasePlanningService(BaseService):
     async def _trim_by_limit(self, decoded_test, facts):
         limited_facts = []
         for limit in re.findall(self.re_limited, decoded_test):
-            limited = copy.deepcopy(facts[0])
+            limited = copy.deepcopy(facts)
             trait = re.search(self.re_trait, limit).group(0)
 
             limit_definitions = re.search(self.re_index, limit).group(0)
             if limit_definitions:
                 for limiter in limit_definitions.split(','):
                     limited = self._apply_limiter(trait=trait, limiter=limiter.split('='), facts=limited)
-            limited_facts.append(limited)
+            if limited:
+                limited_facts.append(limited[0])
         if limited_facts:
             return limited_facts
         return facts
