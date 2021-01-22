f177982c7881cd17d00ece9f115293a353592fee
Fred <59877971+mrengstrom@users.noreply.github.com>
2021-01-22 14:04:34 -0600

    removing self versioning of the caldera app through source file hashing as it is no longer used for its specific purpose and will be replaced with another versioning scheme at a later date (#2013)

--- app/api/rest_api.py
+++ app/api/rest_api.py
@@ -60,7 +60,7 @@ class RestApi(BaseWorld):
         if not access:
             return render_template('login.html', request, dict())
         plugins = await self.data_svc.locate('plugins', {'access': tuple(access), **dict(enabled=True)})
-        data = dict(plugins=[p.display for p in plugins], errors=self.app_svc.errors + self._request_errors(request), version=self.app_svc.version)
+        data = dict(plugins=[p.display for p in plugins], errors=self.app_svc.errors + self._request_errors(request))
         return render_template('%s.html' % access[0].name, request, data)
 
     """ API ENDPOINTS """
