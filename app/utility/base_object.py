8294a38ba36173c377c6347799510f78dfc86828
mchan143 <61020544+mchan143@users.noreply.github.com>
2021-01-26 15:33:45 -0500

    check adversary profile for repeatable abilities and warn user if selected with atomic

--- app/utility/base_object.py
+++ app/utility/base_object.py
@@ -29,7 +29,7 @@ class BaseObject(BaseWorld):
             return self
 
     def update(self, field, value):
-        if (value or type(value) == list) and (value != self.__getattribute__(field)):
+        if (value is not None) and (value != self.__getattribute__(field)):
             self.__setattr__(field, value)
 
     def search_tags(self, value):
