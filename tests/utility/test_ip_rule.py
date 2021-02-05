51fb3af544efc4f3e5c6af333da8ef85fd4430cc
raviserota <ravi.serota@gmail.com>
2021-02-05 06:04:39 -0800

    Changed quote type to meet style guidelines

--- tests/utility/test_ip_rule.py
+++ tests/utility/test_ip_rule.py
@@ -5,19 +5,19 @@ from app.utility.rule_set import RuleSet
 
 
 class TestIPRule:
-    host1 = "127.0.0.1"
-    host2 = "127.0.1.0"
-    host3 = "128.0.0.1"
-    host4 = "127.0.0.0/23"
-    host5 = "127.0.0.0/25"
-    subnet1 = "127.0.0.0/24"
-    fact1 = Fact(trait="host.ip.address", value=host1)
-    fact2 = Fact(trait="host.ip.address", value=host2)
-    fact3 = Fact(trait="host.ip.address", value=host3)
-    fact4 = Fact(trait="host.ip.address", value=host4)
-    fact5 = Fact(trait="host.ip.address", value=host5)
-    fact6 = Fact(trait="host.ip.address", value=subnet1)
-    rule = Rule(trait="host.ip.address", action=RuleAction.DENY, match=subnet1)
+    host1 = '127.0.0.1'
+    host2 = '127.0.1.0'
+    host3 = '128.0.0.1'
+    host4 = '127.0.0.0/23'
+    host5 = '127.0.0.0/25'
+    subnet1 = '127.0.0.0/24'
+    fact1 = Fact(trait='host.ip.address', value=host1)
+    fact2 = Fact(trait='host.ip.address', value=host2)
+    fact3 = Fact(trait='host.ip.address', value=host3)
+    fact4 = Fact(trait='host.ip.address', value=host4)
+    fact5 = Fact(trait='host.ip.address', value=host5)
+    fact6 = Fact(trait='host.ip.address', value=subnet1)
+    rule = Rule(trait='host.ip.address', action=RuleAction.DENY, match=subnet1)
     rs = RuleSet(rules=[rule])
 
     async def test_is_ip_rule_match(self):
