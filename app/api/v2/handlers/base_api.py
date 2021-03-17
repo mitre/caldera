6e6cde63b0d8304caf4c90ae743a11880763b90f
Bryan Worrell <bworrell@mitre.org>
2021-03-17 08:44:55 -0400

    Updates to default logger creation in base_api.

--- app/api/v2/handlers/base_api.py
+++ app/api/v2/handlers/base_api.py
@@ -4,12 +4,12 @@ import logging
 from aiohttp import web
 
 
-DEFAULT_LOGGER_NAME = "rest_api"
+DEFAULT_LOGGER_NAME = 'rest_api'
 
 
 class BaseApi(abc.ABC):
     def __init__(self, logger=None):
-        self._logger = logger or self._create_logger()
+        self._logger = logger or self._create_default_logger()
 
     @property
     def logger(self):
@@ -19,5 +19,5 @@ class BaseApi(abc.ABC):
     def add_routes(self, app: web.Application):
         pass
 
-    def _create_logger(self):
+    def _create_default_logger(self):
         return logging.getLogger(DEFAULT_LOGGER_NAME)
