2412ba24e51cdb40d41bc86f2af7db6cdcadd463
Mitch Kucia <mkucia@mitre.org>
2021-04-18 17:42:18 -0400

    Fix agent tests

--- tests/objects/test_agent.py
+++ tests/objects/test_agent.py
@@ -2,6 +2,7 @@ from base64 import b64decode
 
 from app.objects.c_ability import Ability
 from app.objects.c_agent import Agent
+from app.objects.secondclass.c_executor import Executor
 from app.objects.secondclass.c_fact import Fact
 from app.utility.base_world import BaseWorld
 
@@ -9,8 +10,8 @@ from app.utility.base_world import BaseWorld
 class TestAgent:
 
     def test_task_no_facts(self, loop, data_svc, obfuscator, init_base_world):
-        ability = Ability(ability_id='123', test=BaseWorld.encode_string('whoami'), variations=[],
-                          executor='psh', platform='windows')
+        executor = Executor(name='psh', platform='windows', command='whoami')
+        ability = Ability(ability_id='123', executors=[executor])
         agent = Agent(paw='123', sleep_min=2, sleep_max=8, watchdog=0, executors=['pwsh', 'psh'], platform='windows')
         loop.run_until_complete(agent.task([ability], obfuscator='plain-text'))
         assert 1 == len(agent.links)
@@ -23,8 +24,8 @@ class TestAgent:
         assert 0 == len(agent.links)
 
     def test_task_with_facts(self, loop, obfuscator, init_base_world):
-        ability = Ability(ability_id='123', test=BaseWorld.encode_string('net user #{domain.user.name} /domain'),
-                          variations=[], executor='psh', platform='windows')
+        executor = Executor(name='psh', platform='windows', command='net user #{domain.user.name} /domain')
+        ability = Ability(ability_id='123', executors=[executor])
         agent = Agent(paw='123', sleep_min=2, sleep_max=8, watchdog=0, executors=['pwsh', 'psh'], platform='windows')
         fact = Fact(trait='domain.user.name', value='bob')
 
@@ -32,10 +33,9 @@ class TestAgent:
         assert 1 == len(agent.links)
 
     def test_builtin_fact_replacement(self, loop, obfuscator, init_base_world):
-        ability = Ability(ability_id='123', variations=[], executor='psh', platform='windows',
-                          test=BaseWorld.encode_string(
-                              'echo #{paw} #{server} #{group} #{location} #{exe_name} #{upstream_dest}'
-                          ))
+        executor = Executor(name='psh', platform='windows',
+                            command='echo #{paw} #{server} #{group} #{location} #{exe_name} #{upstream_dest}')
+        ability = Ability(ability_id='123', executors=[executor])
         agent = Agent(paw='123', sleep_min=2, sleep_max=8, watchdog=0, executors=['pwsh', 'psh'], platform='windows',
                       group='my_group', server='http://localhost:8888', location='testlocation', exe_name='testexe')
         loop.run_until_complete(agent.task([ability], 'plain-text', []))
@@ -46,10 +46,9 @@ class TestAgent:
         assert want == decoded_command
 
     def test_builtin_fact_replacement_with_upstream_dest(self, loop, obfuscator, init_base_world):
-        ability = Ability(ability_id='123', variations=[], executor='psh', platform='windows',
-                          test=BaseWorld.encode_string(
-                              'echo #{paw} #{server} #{group} #{location} #{exe_name} #{upstream_dest}'
-                          ))
+        executor = Executor(name='psh', platform='windows',
+                            command='echo #{paw} #{server} #{group} #{location} #{exe_name} #{upstream_dest}')
+        ability = Ability(ability_id='123', executors=[executor])
         agent = Agent(paw='123', sleep_min=2, sleep_max=8, watchdog=0, executors=['pwsh', 'psh'], platform='windows',
                       group='my_group', server='http://10.10.10.10:8888', location='testlocation', exe_name='testexe',
                       upstream_dest='http://127.0.0.1:12345')
