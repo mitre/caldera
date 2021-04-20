56d4a1be3552e19afdda9de68675f95b877fb0e7
Govardhen Arunagiri <garunagiri@mitre.org>
2021-04-20 14:23:19 -0400

    updated functions in case input is null

--- static/js/shared.js
+++ static/js/shared.js
@@ -188,14 +188,18 @@ function closeNav() {
 }
 
 function b64EncodeUnicode(str) { //https://stackoverflow.com/a/30106551
+    if (str != null){
         return btoa(encodeURIComponent(str).replace(/%([0-9A-F]{2})/g,
             function toSolidBytes(match, p1) {
                 return String.fromCharCode('0x' + p1);
         }));
-    }
+    } else return null;
+}
 
 function b64DecodeUnicode(str) { //https://stackoverflow.com/a/30106551
-    return decodeURIComponent(atob(str).split('').map(function(c) {
-        return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
-    }).join(''));
+    if (str != null) {
+        return decodeURIComponent(atob(str).split('').map(function(c) {
+            return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+        }).join(''));
+    } else return "";
 }
