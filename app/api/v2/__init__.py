7aab484c835018041b64d1666ea8c29df006934c
Bryan Worrell <bworrell@mitre.org>
2021-05-06 17:01:54 -0400

    Install json_request_validation_middleware

--- app/api/v2/__init__.py
+++ app/api/v2/__init__.py
@@ -2,10 +2,14 @@ from aiohttp import web
 
 
 def make_app(services):
+    from .responses import json_request_validation_middleware
     from .security import authentication_required_middleware_factory
 
     app = web.Application(
-        middlewares=[authentication_required_middleware_factory(services['auth_svc'])]
+        middlewares=[
+            authentication_required_middleware_factory(services['auth_svc']),
+            json_request_validation_middleware
+        ]
     )
 
     from .handlers.health_api import HealthApi
