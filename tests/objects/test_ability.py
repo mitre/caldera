9ba1001af3c30be4868d3104dfa167eead133c17
Mitch Kucia <mkucia@mitre.org>
2021-05-07 19:24:43 -0400

    Add ability executor tests

--- tests/objects/test_ability.py
+++ tests/objects/test_ability.py
@@ -51,3 +51,38 @@ class TestAbility:
             Ability(ability_id='123')
         ))
         assert agent.privileged_to_run(ability)
+
+    def test_executor_storage(self, loop, ability, executor):
+        test_executor = executor()
+        test_ability = ability(executors=[test_executor])
+
+        assert len(list(test_ability.executors)) == 1
+
+    def test_executor_search(self, loop, ability, executor):
+        test_executor = executor(name='psh', platform='windows')
+        test_ability = ability(executors=[test_executor])
+
+        found_executor = test_ability.find_executor(name='psh', platform='windows')
+        assert found_executor is test_executor
+
+    def test_executor_search_duplicate(self, loop, ability, executor):
+        test_executor = executor(name='psh', platform='windows')
+        test_ability = ability(executors=[test_executor])
+
+        found_executors = test_ability.find_executors(names=['psh', 'psh'], platform='windows')
+        assert len(found_executors) == 1
+        assert found_executors[0] is test_executor
+
+    def test_executor_overwrite(self, loop, ability, executor):
+        test_executor_1 = executor()
+        test_executor_2 = executor()
+        test_ability = ability(executors=[test_executor_1, test_executor_2])
+
+        assert len(list(test_ability.executors)) == 1
+        assert next(test_ability.executors) is test_executor_2
+
+    def test_executor_removal(self, loop, ability, executor):
+        test_ability = ability(executors=[executor()])
+        test_ability.remove_all_executors()
+
+        assert len(list(test_ability.executors)) == 0
