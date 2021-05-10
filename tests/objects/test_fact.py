ee15e50cef796c372263818fdee0cf25443a0acd
Emmanuelle Vargas-Gonzalez <emmanuelle@mitre.org>
2021-05-10 15:05:04 -0400

    add new tests for Fact changes

--- tests/objects/test_fact.py
+++ tests/objects/test_fact.py
@@ -1,4 +1,4 @@
-from app.objects.secondclass.c_fact import Fact
+from app.objects.secondclass.c_fact import Fact, Restriction, Source, Type
 
 
 class TestFact:
@@ -18,3 +18,39 @@ class TestFact:
         test_fact = Fact('test', 'test value| &')
         assert test_fact.escaped('psh') == 'test` value`|` `&'
         assert test_fact.escaped('psh') != 'test value| &'
+
+    def test_fact_trait_and_name_equals(self):
+        test_fact = Fact('test', 'test value')
+        assert test_fact.trait == test_fact.name
+
+    def test_fact_trait_change_equals(self):
+        expected_value = 'another value'
+        test_fact = Fact('test', 'test value')
+        test_fact.trait = expected_value
+        assert test_fact.name == expected_value
+        assert test_fact.trait == expected_value
+
+    def test_fact_name_change_equals(self):
+        expected_value = 'another value'
+        test_fact = Fact('test', 'test value')
+        test_fact.name = expected_value
+        assert test_fact.trait == expected_value
+        assert test_fact.name == expected_value
+
+    def test_fact_restriction(self):
+        test_fact = Fact('test', 'test value', restriction=Restriction.UNIQUE)
+        fact_display = test_fact.display
+        assert test_fact.restriction == Restriction.UNIQUE
+        assert fact_display['restriction'] == 0
+
+    def test_fact_source(self):
+        test_fact = Fact('test', 'test value', source=Source.YAML)
+        fact_display = test_fact.display
+        assert test_fact.source == Source.YAML
+        assert fact_display['source'] == 0
+
+    def test_fact_type(self):
+        test_fact = Fact('test', 'test value', type=Type.DOMAIN)
+        fact_display = test_fact.display
+        assert test_fact.type == Type.DOMAIN
+        assert fact_display['type'] == 0
