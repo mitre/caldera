0d261f5f8135ff0b4202028f7eaff558dfacd50c
blee <blee@mitre.org>
2021-06-22 14:41:34 -0400

    save progress on create/update operation link

--- app/api/v2/handlers/operation_api.py
+++ app/api/v2/handlers/operation_api.py
@@ -3,7 +3,6 @@ from aiohttp import web
 
 from app.api.v2.handlers.base_object_api import BaseObjectApi
 from app.api.v2.managers.operation_api_manager import OperationApiManager
-from app.api.v2.responses import JsonHttpNotFound
 from app.api.v2.schemas.base_schemas import BaseGetAllQuerySchema, BaseGetOneQuerySchema
 from app.objects.c_operation import Operation, OperationSchema
 
@@ -94,13 +93,15 @@ class OperationApi(BaseObjectApi):
         operation_id = request.match_info.get('id')
         link_id = request.match_info.get('link_id')
         link = await self._api_manager.get_operation_link(operation_id, link_id)
-        if not link:
-            raise JsonHttpNotFound(f'Link {link_id} was not found in Operation {operation_id}')
         return web.json_response(link)
 
     @aiohttp_apispec.docs(tags=['operations'])
     async def create_or_update_operation_link(self, request: web.Request):
-        pass
+        operation_id = request.match_info.get('id')
+        link_id = request.match_info.get('link_id')
+        data = await request.json()
+        link = await self._api_manager.create_or_update_operation_link(operation_id, link_id, data)
+        return web.json_response(link)
 
     @aiohttp_apispec.docs(tags=['operations'])
     async def create_potential_links(self, request: web.Request):
@@ -109,7 +110,7 @@ class OperationApi(BaseObjectApi):
     @aiohttp_apispec.docs(tags=['operations'])
     async def get_potential_links(self, request: web.Request):
         operation_id = request.match_info.get('id')
-        potential_links = self._api_manager.get_potential_links(operation_id)
+        potential_links = await self._api_manager.get_potential_links(operation_id)
         return web.json_response(potential_links)
 
     @aiohttp_apispec.docs(tags=['operations'])
@@ -117,6 +118,4 @@ class OperationApi(BaseObjectApi):
         operation_id = request.match_info.get('id')
         paw = request.match_info.get('paw')
         potential_link = await self._api_manager.get_potential_link(operation_id, paw)
-        if not potential_link:
-            raise JsonHttpNotFound(f'Potential link {paw} was not found in Operation {operation_id}')
         return web.json_response(potential_link)
--- app/api/v2/managers/operation_api_manager.py
+++ app/api/v2/managers/operation_api_manager.py
@@ -1,4 +1,6 @@
 from app.api.v2.managers.base_api_manager import BaseApiManager
+from app.objects.secondclass.c_link import LinkSchema
+from app.api.v2.responses import JsonHttpNotFound
 
 
 class OperationApiManager(BaseApiManager):
@@ -6,35 +8,74 @@ class OperationApiManager(BaseApiManager):
         super().__init__(data_svc=data_svc, file_svc=file_svc)
 
     async def get_operation_report(self, operation_id: str):
-        operation = (await self._data_svc.locate('operations', {'id': operation_id}))[0]
+        try:
+            operation = (await self._data_svc.locate('operations', {'id': operation_id}))[0]
+        except Exception:
+            raise JsonHttpNotFound(f'Operation {operation_id} was not found.')
         report = await operation.report(file_svc=self._file_svc, data_svc=self._data_svc)
 
         return report
 
     async def get_operation_links(self, operation_id: str):
-        operation = (await self._data_svc.locate('operations', {'id': operation_id}))[0]
+        try:
+            operation = (await self._data_svc.locate('operations', {'id': operation_id}))[0]
+        except Exception:
+            raise JsonHttpNotFound(f'Operation {operation_id} was not found.')
         links = [link.display for link in operation.chain]
 
         return links
 
     async def get_operation_link(self, operation_id: str, link_id: str):
-        operation = (await self._data_svc.locate('operations', {'id': operation_id}))[0]
+        try:
+            operation = (await self._data_svc.locate('operations', {'id': operation_id}))[0]
+        except Exception:
+            raise JsonHttpNotFound(f'Operation {operation_id} was not found.')
 
         for link in operation.chain:
             if link.id == link_id:
                 return link.display
-        return None
+        raise JsonHttpNotFound(f'Link {link_id} was not found in Operation {operation_id}')
+
+    async def create_or_update_operation_link(self, operation_id: str, link_id: str, data: dict):
+        try:
+            operation = (await self._data_svc.locate('operations', {'id': operation_id}))[0]
+        except Exception:
+            raise JsonHttpNotFound(f'Operation {operation_id} was not found.')
+
+        access = None
+        link = None
+        for entry in operation.chain:
+            if entry.id == link_id:
+                link = entry
+        if not link:
+            raise JsonHttpNotFound(f'Link {link_id} was not found in Operation {operation_id}')
+
+        new_link = self.create_object_from_schema(LinkSchema, data, access)
+        for entry in operation.chain:
+            if entry.id == link_id:
+                del entry
+        operation.chain.append(new_link)
+        return new_link
+
+    async def create_potential_links(self, operation_id: str, link_id: str, data: dict):
+        pass
 
     async def get_potential_links(self, operation_id: str):
-        operation = (await self._data_svc.locate('operations', {'id': operation_id}))[0]
+        try:
+            operation = (await self._data_svc.locate('operations', {'id': operation_id}))[0]
+        except Exception:
+            raise JsonHttpNotFound(f'Operation {operation_id} was not found.')
         potential_links = [potential_link.display for potential_link in operation.potential_links]
 
         return potential_links
 
     async def get_potential_link(self, operation_id: str, paw: str):
-        operation = (await self._data_svc.locate('operations', {'id': operation_id}))[0]
+        try:
+            operation = (await self._data_svc.locate('operations', {'id': operation_id}))[0]
+        except Exception:
+            raise JsonHttpNotFound(f'Operation {operation_id} was not found.')
 
         for link in operation.potential_links:
             if link.paw == paw:
                 return link.display
-        return None
+        raise JsonHttpNotFound(f'Potential link {paw} was not found in Operation {operation_id}')
