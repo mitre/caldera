821ef18081501f42c345caebff7d32849f1e27bd
blee <blee@mitre.org>
2021-07-08 14:29:43 -0400

    reemove createe/update changes for links, potential links, operations for smaller PR

--- app/api/v2/handlers/operation_api.py
+++ app/api/v2/handlers/operation_api.py
@@ -4,7 +4,6 @@ from aiohttp import web
 from app.api.v2.handlers.base_object_api import BaseObjectApi
 from app.api.v2.managers.operation_api_manager import OperationApiManager
 from app.api.v2.schemas.base_schemas import BaseGetAllQuerySchema, BaseGetOneQuerySchema
-from app.objects.secondclass.c_link import LinkSchema
 from app.objects.c_operation import Operation, OperationSchema
 
 
@@ -18,20 +17,9 @@ class OperationApi(BaseObjectApi):
         router = app.router
         router.add_get('/operations', self.get_operations)
         router.add_get('/operations/{id}', self.get_operation_by_id)
-        router.add_post('/operations', self.create_operation)
-        router.add_put('/operations/{id}', self.create_or_update_operation)
-        router.add_patch('/operations/{id}', self.update_operation)
         router.add_delete('/operations/{id}', self.delete_operation)
 
         router.add_get('/operations/{id}/report', self.get_operation_report)
-
-        router.add_get('/operations/{id}/links', self.get_operation_links)
-        router.add_get('/operations/{id}/links/{link_id}', self.get_operation_link)
-        router.add_put('/operations/{id}/links/{link_id}', self.create_or_update_operation_link)
-
-        router.add_post('/operations/{id}/potential-links', self.create_potential_link)
-        router.add_get('/operations/{id}/potential-links', self.get_potential_links)
-        router.add_get('/operations/{id}/potential-links/{paw}', self.get_potential_links_by_paw)
 
     @aiohttp_apispec.docs(tags=['operations'])
     @aiohttp_apispec.querystring_schema(BaseGetAllQuerySchema)
@@ -48,27 +36,6 @@ class OperationApi(BaseObjectApi):
         return web.json_response(operation)
 
     @aiohttp_apispec.docs(tags=['operations'])
-    @aiohttp_apispec.request_schema(OperationSchema)
-    @aiohttp_apispec.response_schema(OperationSchema)
-    async def create_operation(self, request: web.Request):
-        operation = await self.create_object(request)
-        return web.json_response(operation.display)
-
-    @aiohttp_apispec.docs(tags=['operations'])
-    @aiohttp_apispec.request_schema(OperationSchema(partial=True))
-    @aiohttp_apispec.response_schema(OperationSchema)
-    async def create_or_update_operation(self, request: web.Request):
-        operation = await self.create_or_update_object(request)
-        return web.json_response(operation.display)
-
-    @aiohttp_apispec.docs(tags=['operations'])
-    @aiohttp_apispec.request_schema(OperationSchema(partial=True))
-    @aiohttp_apispec.response_schema(OperationSchema(partial=True))
-    async def update_operation(self, request: web.Request):
-        operation = await self.update_object(request)
-        return web.json_response(operation.display)
-
-    @aiohttp_apispec.docs(tags=['operations'])
     @aiohttp_apispec.response_schema(OperationSchema)
     async def delete_operation(self, request: web.Request):
         await self.delete_object(request)
@@ -81,62 +48,3 @@ class OperationApi(BaseObjectApi):
         access = await self.get_request_permissions(request)
         report = await self._api_manager.get_operation_report(operation_id, access)
         return web.json_response(report)
-
-    @aiohttp_apispec.docs(tags=['operations'])
-    @aiohttp_apispec.querystring_schema(BaseGetAllQuerySchema)
-    @aiohttp_apispec.response_schema(LinkSchema(many=True, partial=True))
-    async def get_operation_links(self, request: web.Request):
-        operation_id = request.match_info.get('id')
-        access = await self.get_request_permissions(request)
-        links = await self._api_manager.get_operation_links(operation_id, access)
-        return web.json_response(links)
-
-    @aiohttp_apispec.docs(tags=['operations'])
-    @aiohttp_apispec.querystring_schema(BaseGetOneQuerySchema)
-    @aiohttp_apispec.response_schema(LinkSchema(partial=True))
-    async def get_operation_link(self, request: web.Request):
-        operation_id = request.match_info.get('id')
-        link_id = request.match_info.get('link_id')
-        access = await self.get_request_permissions(request)
-        link = await self._api_manager.get_operation_link(operation_id, link_id, access)
-        return web.json_response(link)
-
-    @aiohttp_apispec.docs(tags=['operations'])
-    @aiohttp_apispec.request_schema(LinkSchema(partial=True))
-    @aiohttp_apispec.response_schema(LinkSchema)
-    async def create_or_update_operation_link(self, request: web.Request):
-        operation_id = request.match_info.get('id')
-        link_id = request.match_info.get('link_id')
-        access = await self.get_request_permissions(request)
-        data = await request.json()
-        link = await self._api_manager.create_or_update_operation_link(operation_id, link_id, data, access)
-        return web.json_response(link)
-
-    @aiohttp_apispec.docs(tags=['operations'])
-    @aiohttp_apispec.request_schema(LinkSchema)
-    @aiohttp_apispec.response_schema(LinkSchema)
-    async def create_potential_link(self, request: web.Request):
-        operation_id = request.match_info.get('id')
-        access = await self.get_request_permissions(request)
-        data = await request.json()
-        potential_link = await self._api_manager.create_potential_link(operation_id, data, access)
-        return web.json_response(potential_link)
-
-    @aiohttp_apispec.docs(tags=['operations'])
-    @aiohttp_apispec.querystring_schema(BaseGetAllQuerySchema)
-    @aiohttp_apispec.response_schema(LinkSchema(many=True, partial=True))
-    async def get_potential_links(self, request: web.Request):
-        operation_id = request.match_info.get('id')
-        access = await self.get_request_permissions(request)
-        potential_links = await self._api_manager.get_potential_links(operation_id, access)
-        return web.json_response(potential_links)
-
-    @aiohttp_apispec.docs(tags=['operations'])
-    @aiohttp_apispec.querystring_schema(BaseGetOneQuerySchema)
-    @aiohttp_apispec.response_schema(LinkSchema(partial=True))
-    async def get_potential_links_by_paw(self, request: web.Request):
-        operation_id = request.match_info.get('id')
-        paw = request.match_info.get('paw')
-        access = await self.get_request_permissions(request)
-        potential_links = await self._api_manager.get_potential_links_by_paw(operation_id, paw, access)
-        return web.json_response(potential_links)
--- app/api/v2/managers/operation_api_manager.py
+++ app/api/v2/managers/operation_api_manager.py
@@ -1,9 +1,5 @@
-from marshmallow.schema import SchemaMeta
-
 from app.api.v2.managers.base_api_manager import BaseApiManager
-from app.api.v2.responses import JsonHttpNotFound, JsonHttpForbidden, JsonHttpBadRequest
-from app.objects.secondclass.c_link import LinkSchema
-from app.utility.base_world import BaseWorld
+from app.api.v2.responses import JsonHttpNotFound, JsonHttpForbidden
 
 
 class OperationApiManager(BaseApiManager):
@@ -11,80 +7,17 @@ class OperationApiManager(BaseApiManager):
         super().__init__(data_svc=data_svc, file_svc=file_svc)
 
     async def get_operation_report(self, operation_id: str, access: dict):
-        operation = await self.get_operation(operation_id, access)
+        operation = await self.get_operation_object(operation_id, access)
         report = await operation.report(file_svc=self._file_svc, data_svc=self._data_svc)
 
         return report
 
-    async def get_operation_links(self, operation_id: str, access: dict):
-        operation = await self.get_operation(operation_id, access)
-        links = [link.display for link in operation.chain]
-
-        return links
-
-    async def get_operation_link(self, operation_id: str, link_id: str, access: dict):
-        operation = await self.get_operation(operation_id, access)
-        for link in operation.chain:
-            if link.id == link_id:
-                return link.display
-        raise JsonHttpNotFound(f'Link {link_id} was not found in Operation {operation_id}')
-
-    async def create_or_update_operation_link(self, operation_id: str, link_id: str,
-                                              link_data: dict, access: BaseWorld.Access):
-        operation = await self.get_operation(operation_id, access)
-        existing_link = None
-        for entry in operation.chain:
-            if entry.id == link_id:
-                existing_link = entry
-                break
-        if existing_link and existing_link.access not in access['access']:
-            raise JsonHttpForbidden(f'Cannot update link {link_id} due to insufficient permissions.')
-
-        new_link = self.create_secondclass_object_from_schema(LinkSchema, link_data, access)
-        if existing_link:
-            operation.chain.remove(entry)
-        operation.chain.append(new_link)
-        return new_link.display
-
-    async def create_potential_link(self, operation_id: str, link_data: dict, access: BaseWorld.Access):
-        operation = await self.get_operation(operation_id, access)
-        link_id = link_data['id']
-        for entry in operation.potential_links:
-            if entry.id == link_id:
-                raise JsonHttpBadRequest(f'Link with given id already exists: {link_id}')
-        new_link = self.create_secondclass_object_from_schema(LinkSchema, link_data, access)
-        operation.potential_links.append(new_link)
-        return new_link.display
-
-    async def get_potential_links(self, operation_id: str, access: dict):
-        operation = await self.get_operation(operation_id, access)
-        potential_links = [potential_link.display for potential_link in operation.potential_links]
-
-        return potential_links
-
-    async def get_potential_links_by_paw(self, operation_id: str, paw: str, access: dict):
-        operation = await self.get_operation(operation_id, access)
-
-        output_links = []
-        for link in operation.potential_links:
-            if link.paw == paw:
-                output_links.append(link.display)
-        if not output_links:
-            raise JsonHttpNotFound(f'paw {paw} was not found in potential links for Operation {operation_id}')
-        return output_links
-
     """Object Creation Helpers"""
-    async def get_operation(self, operation_id: str, access: dict):
+    async def get_operation_object(self, operation_id: str, access: dict):
         try:
             operation = (await self._data_svc.locate('operations', {'id': operation_id}))[0]
         except Exception:
-            raise JsonHttpNotFound(f'Operation {operation_id} was not found.')
+            raise JsonHttpNotFound(f'Operation not found: {operation_id}')
         if operation.match(access):
             return operation
-        raise JsonHttpForbidden(f'Insufficient permissions to view operation {operation_id}')
-
-    def create_secondclass_object_from_schema(self, schema: SchemaMeta, data: dict, access: BaseWorld.Access):
-        obj_schema = schema()
-        obj = obj_schema.load(data)
-        obj.access = self._get_allowed_from_access(access)
-        return obj
+        raise JsonHttpForbidden(f'Cannot view operation due to insufficient permissions: {operation_id}')
