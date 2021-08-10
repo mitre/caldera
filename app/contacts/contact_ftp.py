ebb2682656d5dd05866fb241ead67b36a21fb4b4
Cassandra Diaz <cdiaz@mitre.org>
2021-08-10 11:11:47 -0500

    Update pull request contact_ftp.py

--- app/contacts/contact_ftp.py
+++ app/contacts/contact_ftp.py
@@ -3,6 +3,7 @@ import os
 import re
 import asyncio
 import aioftp
+import sys
 
 from app.utility.base_world import BaseWorld
 
@@ -24,8 +25,12 @@ class Contact(BaseWorld):
 
     async def start(self):
         self.set_up_server()
-        t = asyncio.create_task(self.ftp_server())
-        await t
+        if sys.version_info >= (3, 7):
+            t = asyncio.create_task(self.ftp_server_python_new())
+            await t
+        else:
+            t = asyncio.create_task(self.ftp_server_python_old())
+            await t
 
     def set_up_server(self):
         # If directory doesn't exist, make the directory
@@ -33,13 +38,13 @@ class Contact(BaseWorld):
             os.makedirs(self.directory)
 
         # Define a new user with full r/w permissions
-        u = (
+        user = (
             aioftp.User(
                 str(self.user),
                 str(self.pword),
                 home_path=self.directory,
                 permissions=(
-                    aioftp.Permission("/", readable=False, writable=False),
+                    aioftp.Permission('/', readable=False, writable=False),
                     aioftp.Permission(self.directory, readable=True, writable=True),
                 ),
                 maximum_connections=256,
@@ -49,10 +54,10 @@ class Contact(BaseWorld):
                 write_speed_limit_per_connection=100 * 1024
             ),
             aioftp.User(
-                home_path="/anon",
+                home_path='/anon',
                 permissions=(
-                    aioftp.Permission("/", readable=False, writable=False),
-                    aioftp.Permission("/anon", readable=True),
+                    aioftp.Permission('/', readable=False, writable=False),
+                    aioftp.Permission('/anon', readable=True),
                 ),
                 maximum_connections=5,
                 read_speed_limit=1024 * 1024,
@@ -62,25 +67,27 @@ class Contact(BaseWorld):
             ),
         )
         # Instantiate FTP server on local host and listen on 1026
-        self.server = MyServer(u, self.contact_svc, self.file_svc, self.logger, self.host, self.port, self.user,
+        self.server = MyServer(user, self.contact_svc, self.file_svc, self.logger, self.host, self.port, self.user,
                                self.pword, self.directory, self.home)
 
-    async def ftp_server(self):
+    async def ftp_server_python_old(self):
         await self.server.start(host=self.host, port=self.port)
 
+    async def ftp_server_python_new(self):
+        await self.server.run(host=self.host, port=self.port)
 
 class MyServer(aioftp.Server):
-    def __init__(self, u, contact, file, log, host_ip, port_in, user, password, u_dir, start,
+    def __init__(self, user, contact, file, log, host_ip, port_in, username, password, user_dir, start,
                  *,  max_con=256):
-        super().__init__(u, maximum_connections=max_con)
+        super().__init__(user, maximum_connections=max_con)
         self.contact_svc = contact
         self.file_svc = file
         self.logger = log
         self.host = host_ip
         self.port = port_in
-        self.login = user
+        self.login = username
         self.pword = password
-        self.directory = u_dir
+        self.directory = user_dir
         self.home = start
 
     @aioftp.ConnectionConditions(
@@ -96,6 +103,7 @@ class MyServer(aioftp.Server):
             fail_info="Can't open data connection")
         @aioftp.worker
         async def stor_worker(self, connection, rest):
+            bytes_obj = b''
             stream = connection.data_connection
             del connection.data_connection
             if connection.restart_offset:
@@ -104,43 +112,20 @@ class MyServer(aioftp.Server):
                 file_mode = mode
             file_out = connection.path_io.open(real_path, mode=file_mode)
 
-            bytes_obj = b''
-            file = False
-            p_load = False
-
             async with file_out, stream:
                 if connection.restart_offset:
                     await file_out.seek(connection.restart_offset)
                 async for data in stream.iter_by_block(connection.block_size):
                     bytes_obj += data
 
-            if re.match(r"^Alive\.txt$", name[len(name) - 1]):
-                file = True
-                profile = json.loads(bytes_obj.decode())
-                paw, response = await r_class.create_response(profile)
-                success = r_class.write_response_file(paw, response)
-                if not success:
-                    self.logger.debug("ERROR: Failed to create response")
-
-            if re.match(r"^Payload\.txt$", name[len(name) - 1]):
-                p_load = True
-                profile = json.loads(bytes_obj.decode())
-                file_path, contents, display_name = await r_class.get_payload_file(profile)
-                if file_path is not None:
-                    r_class.write_file(profile.get('paw'), profile.get('file'), str(contents))
-
-            if not file and not p_load:
-                paw = name[len(name) - 2]
-                filename = name[len(name) - 1]
-                await r_class.submit_uploaded_file(paw, filename, bytes_obj)
-
+            await self.handle_agent_file(name, bytes_obj, r_class)
             connection.response("226", "data transfer done")
             del stream
             return True
 
         real_path, virtual_path = self.get_paths(connection, rest)
         name = str(virtual_path).split("/")
-        self.logger.debug("File: " + str(name[len(name) - 1]) + " received")
+        self.logger.debug("File: " + str(name[-1]) + " received")
         r_class = CalderaServer(self.contact_svc, self.file_svc, self.logger, self.home, self.directory)
 
         if await connection.path_io.is_dir(real_path.parent):
@@ -153,16 +138,38 @@ class MyServer(aioftp.Server):
         connection.response(code, info)
         return True
 
+    async def handle_agent_file(self, file_name, file_bytes, r_class):
+        p_load = file = False
+        if re.match(r"^Alive\.txt$", file_name[-1]):
+            file = True
+            profile = json.loads(file_bytes.decode())
+            paw, response = await r_class.create_beacon_response(profile)
+            success = r_class.write_beacon_response_file(paw, response)
+            if not success:
+                self.logger.debug("ERROR: Failed to create response")
+
+        if re.match(r"^Payload\.txt$", file_name[-1]):
+            p_load = True
+            profile = json.loads(file_bytes.decode())
+            file_path, contents, display_name = await r_class.get_payload_file(profile)
+            if file_path is not None:
+                r_class.write_payload_file(profile.get('paw'), profile.get('file'), str(contents))
+
+        if not file and not p_load:
+            paw = file_name[-2]
+            filename = file_name[-1]
+            await r_class.submit_uploaded_file(paw, filename, file_bytes)
+
 
 class CalderaServer:
-    def __init__(self, contact, file, log, h, d):
+    def __init__(self, contact, file, log, home, directory):
         self.contact_svc = contact
         self.file_svc = file
         self.logger = log
-        self.home = h
-        self.directory = d
+        self.home = home
+        self.directory = directory
 
-    async def create_response(self, profile):
+    async def create_beacon_response(self, profile):
         paw = profile.get('paw')
         profile['paw'] = paw
         profile['contact'] = profile.get('contact', 'ftp')
@@ -175,9 +182,9 @@ class CalderaServer:
             response['new_contact'] = agent.pending_contact
             self.logger.debug('Sending agent instructions to switch from C2 channel %s to %s'
                               % (agent.contact, agent.pending_contact))
-        return paw, response
+        return agent.paw, response
 
-    def write_response_file(self, paw, response):
+    def write_beacon_response_file(self, paw, response):
         filename = str(self.home + self.directory)
         try:
             if not os.path.exists(filename):
@@ -193,7 +200,7 @@ class CalderaServer:
             return False, "", ""
         return True
 
-    def write_file(self, paw, file_name, contents):
+    def write_payload_file(self, paw, file_name, contents):
         filename = str(self.home + self.directory)
         try:
             if not os.path.exists(filename):
