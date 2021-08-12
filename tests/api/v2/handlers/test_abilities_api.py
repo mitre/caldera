4e5cbb79e5ddcbe193ee0d0d65047de1f9e9f12d
blee <blee@mitre.org>
2021-08-12 15:57:21 -0400

    test additional endpoints w/o authorization

--- tests/api/v2/handlers/test_abilities_api.py
+++ tests/api/v2/handlers/test_abilities_api.py
@@ -26,6 +26,10 @@ class TestAbilitiesApi:
         assert ability_dict['ability_id'] == '123'
         assert ability_dict['name'] == 'Test Ability'
 
+    async def test_unauthorized_get_abilities(self, api_client):
+        resp = await api_client.get('/api/v2/abilities')
+        assert resp.status == HTTPStatus.UNAUTHORIZED
+
     async def test_get_ability_by_id(self, api_client, api_cookies):
         resp = await api_client.get('/api/v2/abilities/123', cookies=api_cookies)
         ability_dict = await resp.json()
@@ -35,14 +39,6 @@ class TestAbilitiesApi:
     async def test_unauthorized_get_ability_by_id(self, api_client):
         resp = await api_client.get('/api/v2/abilities/123')
         assert resp.status == HTTPStatus.UNAUTHORIZED
-
-    async def test_delete_ability_by_id(self, api_client, api_cookies):
-        ability_exists = await BaseService.get_service('data_svc').locate('abilities', {'ability_id': '123'})
-        assert ability_exists
-        resp = await api_client.delete('/api/v2/abilities/123', cookies=api_cookies)
-        assert resp.status == HTTPStatus.NO_CONTENT
-        ability_exists = await BaseService.get_service('data_svc').locate('abilities', {'ability_id': '123'})
-        assert not ability_exists
 
     async def test_create_ability(self, api_client, api_cookies, mocker, async_return):
         test_executor_linux = Executor(name='sh', platform='linux', command='whoami')
@@ -55,6 +51,13 @@ class TestAbilitiesApi:
         ability_exists = await BaseService.get_service('data_svc').locate('abilities', {'ability_id': '456'})
         assert ability_exists
 
+    async def test_unauthorized_create_ability(self, api_client):
+        test_executor_linux = Executor(name='sh', platform='linux', command='whoami')
+        payload = dict(name='new test ability', ability_id='456', tactic='collection', technique_name='collection',
+                       technique_id='1', executors=[ExecutorSchema().dump(test_executor_linux)])
+        resp = await api_client.post('/api/v2/abilities', json=payload)
+        assert resp.status == HTTPStatus.UNAUTHORIZED
+
     async def test_update_ability(self, api_client, api_cookies):
         payload = dict(name='an updated test ability', tactic='defense-evasion')
         resp = await api_client.patch('/api/v2/abilities/123', cookies=api_cookies, json=payload)
@@ -62,6 +65,11 @@ class TestAbilitiesApi:
         ability = (await BaseService.get_service('data_svc').locate('abilities', {'ability_id': '123'}))[0]
         assert ability.name == payload['name']
         assert ability.tactic == payload['tactic']
+
+    async def test_unauthorized_update_ability(self, api_client):
+        payload = dict(name='an updated test ability', tactic='defense-evasion')
+        resp = await api_client.patch('/api/v2/abilities/123', json=payload)
+        assert resp.status == HTTPStatus.UNAUTHORIZED
 
     async def test_replace_ability(self, api_client, api_cookies):
         test_executor_linux = Executor(name='sh', platform='linux', command='whoami')
@@ -74,3 +82,10 @@ class TestAbilitiesApi:
         assert ability['technique_name'] == payload['technique_name']
         assert ability['technique_id'] == payload['technique_id']
         assert ability['tactic'] == payload['tactic']
+
+    async def test_unauthorized_repalce_ability(self, api_client):
+        test_executor_linux = Executor(name='sh', platform='linux', command='whoami')
+        payload = dict(name='replaced test ability', tactic='collection', technique_name='discovery', technique_id='2',
+                       executors=[ExecutorSchema().dump(test_executor_linux)])
+        resp = await api_client.put('/api/v2/abilities/123', json=payload)
+        assert resp.status == HTTPStatus.UNAUTHORIZED
