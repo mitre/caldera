e75928c9b880e4058723f4c129b668fa4169e85f
Anna Lin <alin@mitre.org>
2021-08-25 15:57:55 -0400

    Moved getHumanFriendlyTime() to shared.js

--- static/js/shared.js
+++ static/js/shared.js
@@ -36,6 +36,93 @@ function apiV2(requestType, endpoint, body = null) {
     });
 }
 
+//
+// Parse timestamp into human-friendly date format
+// Modified from original code: https://stackoverflow.com/questions/7641791/javascript-library-for-human-friendly-relative-date-formatting
+//
+// Expected input: (i.e.) 2021-08-25 10:03:23
+// Output: (i.e.) 5 hrs ago; (i.e. if older than 1 day): yesterday 10:03:23; (i.e. if older than 2 days): Aug 25 10:03:23
+//
+function getHumanFriendlyTime(date) {
+    // i.e. format 2021-08-03 19:37:08
+    if (!date) return '';
+    let split = date.split('-');
+
+    let hMonth = Number(split[1]) - 1;
+    let hDate = Number(split[2].split(' ')[0]);
+    let hTime = split[2].split(' ')[1].split(':');
+
+    const givenDate = new Date(split[0], hMonth, hDate, hTime[0], hTime[1], hTime[2]);
+    // Make a fuzzy time
+    let delta = Math.round((new Date - givenDate) / 1000);
+
+    let minute = 60,
+        hour = minute * 60,
+        day = hour * 24;
+
+    let fuzzy;
+
+    if (delta < 30) {
+        fuzzy = 'just now';
+    } else if (delta < minute) {
+        fuzzy = delta + ' seconds ago';
+    } else if (delta < 2 * minute) {
+        fuzzy = 'a minute ago'
+    } else if (delta < hour) {
+        fuzzy = Math.floor(delta / minute) + ' min ago';
+    } else if (Math.floor(delta / hour) === 1) {
+        fuzzy = '1 hr ago'
+    } else if (delta < day) {
+        fuzzy = Math.floor(delta / hour) + ' hrs ago';
+    } else if (delta < day * 2) {
+        fuzzy = 'yesterday ' + (hTime.join(':'));
+    } else {
+        switch (hMonth) {
+            case 0:
+                hMonth = 'Jan';
+                break;
+            case 1:
+                hMonth = 'Feb';
+                break;
+            case 2:
+                hMonth = 'Mar';
+                break;
+            case 3:
+                hMonth = 'Apr';
+                break;
+            case 4:
+                hMonth = 'May';
+                break;
+            case 5:
+                hMonth = 'Jun';
+                break;
+            case 6:
+                hMonth = 'Jul';
+                break;
+            case 7:
+                hMonth = 'Aug';
+                break;
+            case 8:
+                hMonth = 'Sep';
+                break;
+            case 9:
+                hMonth = 'Oct';
+                break;
+            case 10:
+                hMonth = 'Nov';
+                break;
+            case 11:
+                hMonth = 'Dec';
+                break;
+            default:
+                hMonth = '';
+                break;
+        }
+        fuzzy = hMonth + ' ' + hDate + ' ' + hTime.join(':');
+    }
+    return fuzzy;
+}
+
 function sortAlphabetically(list) {
     return list.sort((a, b) => {
         let x = a.toLowerCase(), y = b.toLowerCase();
