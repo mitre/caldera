9fa04621c4683125450e0a0f9f843353b452f74d
blee <blee@mitre.org>
2021-08-25 11:41:33 -0400

    add additional tests

--- tests/api/v2/handlers/test_agents_api.py
+++ tests/api/v2/handlers/test_agents_api.py
@@ -7,6 +7,18 @@ from app.utility.base_service import BaseService
 
 
 @pytest.fixture
+def updated_agent_payload():
+    return {
+        'group': 'blue',
+        'trusted': False,
+        'sleep_min': 1,
+        'sleep_max': 5,
+        'watchdog': 0,
+        'pending_contact': 'HTML'
+    }
+
+
+@pytest.fixture
 def new_agent_payload():
     return {
         'paw': '456',
@@ -75,30 +87,56 @@ class TestAgentsApi:
         resp = await api_v2_client.post('/api/v2/agents', cookies=api_cookies, json=new_agent_payload)
         assert resp.status == HTTPStatus.OK
         agent_dict = await resp.json()
-        assert agent_dict == test_agent.schema.dump(test_agent)
+        assert agent_dict['sleep_min'] == new_agent_payload['sleep_min']
+        assert agent_dict['sleep_max'] == new_agent_payload['sleep_max']
+        assert agent_dict['paw'] == new_agent_payload['paw']
+        assert agent_dict['executors'] == new_agent_payload['executors']
 
     async def test_unauthorized_create_agent(self, api_v2_client, new_agent_payload):
         resp = await api_v2_client.post('/api/v2/agents', json=new_agent_payload)
         assert resp.status == HTTPStatus.UNAUTHORIZED
 
-    async def test_update_agent(self, api_v2_client, api_cookies, test_agent):
-        pass
+    async def test_update_agent(self, api_v2_client, api_cookies, test_agent, updated_agent_payload):
+        resp = await api_v2_client.patch('/api/v2/agents/123', cookies=api_cookies, json=updated_agent_payload)
+        assert resp.status == HTTPStatus.OK
+        agent_dict = await resp.json()
+        assert agent_dict['sleep_min'] == updated_agent_payload['sleep_min']
+        assert agent_dict['sleep_max'] == updated_agent_payload['sleep_max']
+        assert agent_dict['group'] == updated_agent_payload['group']
+        assert agent_dict['trusted'] == updated_agent_payload['trusted']
+        assert agent_dict['pending_contact'] == updated_agent_payload['pending_contact']
 
-    async def test_unauthorized_update_agent(self, api_v2_client, test_agent):
-        pass
+    async def test_unauthorized_update_agent(self, api_v2_client, test_agent, updated_agent_payload):
+        resp = await api_v2_client.patch('/api/v2/agents/123', json=updated_agent_payload)
+        assert resp.status == HTTPStatus.UNAUTHORIZED
 
-    async def test_nonexistent_update_agent(self, api_v2_client, api_cookies):
-        pass
+    async def test_nonexistent_update_agent(self, api_v2_client, api_cookies, updated_agent_payload):
+        resp = await api_v2_client.patch('/api/v2/agents/999', cookies=api_cookies, json=updated_agent_payload)
+        assert resp.status == HTTPStatus.NOT_FOUND
 
-    async def test_create_or_update_agent(self, api_v2_client, api_cookies, test_agent):
-        pass
+    async def test_create_or_update_agent(self, api_v2_client, api_cookies, test_agent, updated_agent_payload):
+        resp = await api_v2_client.put('/api/v2/agents/123', cookies=api_cookies, json=updated_agent_payload)
+        assert resp.status == HTTPStatus.OK
+        agent_dict = await resp.json()
+        assert agent_dict['sleep_min'] == updated_agent_payload['sleep_min']
+        assert agent_dict['sleep_max'] == updated_agent_payload['sleep_max']
+        assert agent_dict['group'] == updated_agent_payload['group']
+        assert agent_dict['trusted'] == updated_agent_payload['trusted']
+        assert agent_dict['pending_contact'] == updated_agent_payload['pending_contact']
 
-    async def test_unauthorized_create_or_update_agent(self, api_v2_client, test_agent):
-        pass
+    async def test_unauthorized_create_or_update_agent(self, api_v2_client, test_agent, updated_agent_payload):
+        resp = await api_v2_client.put('/api/v2/agents/123', json=updated_agent_payload)
+        assert resp.status == HTTPStatus.UNAUTHORIZED
 
-    async def test_nonexistent_create_or_update_agent(self, api_v2_client, api_cookies):
-        pass
-
+    async def test_nonexistent_create_or_update_agent(self, api_v2_client, api_cookies, new_agent_payload):
+        resp = await api_v2_client.put('/api/v2/agents/456', cookies=api_cookies, json=new_agent_payload)
+        assert resp.status == HTTPStatus.OK
+        agent_dict = await resp.json()
+        assert agent_dict['sleep_min'] == new_agent_payload['sleep_min']
+        assert agent_dict['sleep_max'] == new_agent_payload['sleep_max']
+        assert agent_dict['paw'] == new_agent_payload['paw']
+        assert agent_dict['executors'] == new_agent_payload['executors']
+    '''
     async def test_delete_agent(self, api_v2_client, api_cookies, test_agent):
         pass
 
@@ -106,4 +144,6 @@ class TestAgentsApi:
         pass
 
     async def test_unauthorized_get_deploy_commands(self, api_v2_client, test_agent):
-        pass
+        resp = await api_v2_client.patch('/api/v2/deploy_commands/}', json=)
+        assert resp.status == HTTPStatus.FORBIDDEN
+    '''
