07964dd0f38f8875e5e61861e97b96d39bf3f416
blee <blee@mitre.org>
2021-08-30 14:05:35 -0400

    add contact handler + manager

---
+++ app/api/v2/managers/contact_api_manager.py
@@ -0,0 +1,11 @@
+from app.api.v2.managers.base_api_manager import BaseApiManager
+
+
+class ContactApiManager(BaseApiManager):
+    def __init__(self, data_svc, file_svc, contact_svc):
+        super().__init__(data_svc=data_svc, file_svc=file_svc)
+        self.contact_svc = contact_svc
+
+    def get_contact_report(self, contact: str = None):
+        report = self.contact_svc.report  # .get(contact, dict())
+        return report
