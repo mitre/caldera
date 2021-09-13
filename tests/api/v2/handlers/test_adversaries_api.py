0d75ae0ad5fa8148b91dbe7fc33b98070d0f841a
blee <blee@mitre.org>
2021-09-13 13:53:32 -0400

    update fixtures

--- tests/api/v2/handlers/test_adversaries_api.py
+++ tests/api/v2/handlers/test_adversaries_api.py
@@ -2,7 +2,7 @@ import pytest
 
 from http import HTTPStatus
 
-from app.objects.c_adversary import AdversarySchema
+from app.objects.c_adversary import AdversarySchema, Adversary
 from app.utility.base_service import BaseService
 
 
@@ -17,6 +17,13 @@ def updated_adversary_payload():
 
 
 @pytest.fixture
+def expected_updated_adversary_dump(test_adversary, updated_adversary_payload):
+    adversary_dict = test_adversary.schema.dump(test_adversary)
+    adversary_dict.update(updated_adversary_payload)
+    return adversary_dict
+
+
+@pytest.fixture
 def new_adversary_payload():
     return {
         'name': 'test new adversary',
@@ -25,6 +32,12 @@ def new_adversary_payload():
         'objective': '495a9828-cab1-44dd-a0ca-66e58177d8cc',
         'tags': []
     }
+
+
+@pytest.fixture
+def expected_new_adversary_dump(new_adversary_payload):
+    adversary = Adversary.load(new_adversary_payload)
+    return adversary.schema.dump(adversary)
 
 
 @pytest.fixture
@@ -67,16 +80,14 @@ class TestAdversariesApi:
         resp = await api_v2_client.get('/api/v2/adversaries/999', cookies=api_cookies)
         assert resp.status == HTTPStatus.NOT_FOUND
 
-    async def test_create_adversary(self, api_v2_client, api_cookies, test_adversary, new_adversary_payload):
+    async def test_create_adversary(self, api_v2_client, api_cookies, test_adversary, new_adversary_payload,
+                                    expected_new_adversary_dump):
         resp = await api_v2_client.post('/api/v2/adversaries', cookies=api_cookies, json=new_adversary_payload)
         assert resp.status == HTTPStatus.OK
         output = await resp.json()
         assert await BaseService.get_service('data_svc').locate('adversaries',
                                                                 match={'adversary_id': output['adversary_id']})
-        assert output['name'] == new_adversary_payload['name']
-        assert output['description'] == new_adversary_payload['description']
-        assert output['tags'] == new_adversary_payload['tags']
-        assert output['objective'] == new_adversary_payload['objective']
+        assert output == expected_new_adversary_dump
 
     async def test_unauthorized_create_adversary(self, api_v2_client, test_adversary, new_adversary_payload):
         resp = await api_v2_client.post('/api/v2/adversaries', json=new_adversary_payload)
@@ -88,7 +99,7 @@ class TestAdversariesApi:
         assert resp.status == HTTPStatus.BAD_REQUEST
 
     async def test_update_adversary(self, api_v2_client, api_cookies, test_adversary, updated_adversary_payload,
-                                    mocker):
+                                    mocker, expected_updated_adversary_dump):
         with mocker.patch('app.api.v2.managers.adversary_api_manager.AdversaryApiManager.strip_yml') as mock_strip_yml:
             mock_strip_yml.return_value = [test_adversary.schema.dump(test_adversary)]
             with mocker.patch('app.objects.c_adversary.Adversary.verify') as mock_verify:
@@ -97,9 +108,7 @@ class TestAdversariesApi:
                                                  json=updated_adversary_payload)
                 assert resp.status == HTTPStatus.OK
                 output = await resp.json()
-                expected_payload = test_adversary.schema.dump(test_adversary)
-                expected_payload.update(updated_adversary_payload)
-                assert output == expected_payload
+                assert output == expected_updated_adversary_dump
 
     async def test_unauthorized_update_adversary(self, api_v2_client, test_adversary, updated_adversary_payload):
         resp = await api_v2_client.patch('/api/v2/adversaries/123', json=updated_adversary_payload)
@@ -109,30 +118,25 @@ class TestAdversariesApi:
         resp = await api_v2_client.patch('/api/v2/adversaries/999', json=updated_adversary_payload)
         assert resp.status == HTTPStatus.NOT_FOUND
 
-    async def test_create_or_update_existing_adversary(self, api_v2_client, api_cookies, test_adversary,
-                                                       updated_adversary_payload, mocker):
+    async def test_create_or_update_existing_adversary(self, api_v2_client, api_cookies, test_adversary, mocker,
+                                                       updated_adversary_payload, expected_updated_adversary_dump):
         with mocker.patch('app.objects.c_adversary.Adversary.verify') as mock_verify:
             mock_verify.return_value = None
             resp = await api_v2_client.put('/api/v2/adversaries/123', cookies=api_cookies,
                                            json=updated_adversary_payload)
             assert resp.status == HTTPStatus.OK
             output = await resp.json()
-            expected_payload = test_adversary.schema.dump(test_adversary)
-            expected_payload.update(updated_adversary_payload)
-            assert output == expected_payload
+            assert output == expected_updated_adversary_dump
 
     async def test_unauthorized_create_or_update_adversary(self, api_v2_client, test_adversary, new_adversary_payload):
         resp = await api_v2_client.put('/api/v2/adversaries/123', json=new_adversary_payload)
         assert resp.status == HTTPStatus.UNAUTHORIZED
 
     async def test_create_or_update_nonexistent_adversary(self, api_v2_client, api_cookies, test_adversary,
-                                                          new_adversary_payload):
+                                                          new_adversary_payload, expected_new_adversary_dump):
         resp = await api_v2_client.put('/api/v2/adversaries/456', cookies=api_cookies, json=new_adversary_payload)
         assert resp.status == HTTPStatus.OK
         output = await resp.json()
         assert await BaseService.get_service('data_svc').locate('adversaries',
                                                                 match={'adversary_id': output['adversary_id']})
-        assert output['name'] == new_adversary_payload['name']
-        assert output['description'] == new_adversary_payload['description']
-        assert output['objective'] == new_adversary_payload['objective']
-        assert output['tags'] == new_adversary_payload['tags']
+        assert output == expected_new_adversary_dump
