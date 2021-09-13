d33c7ea9c5a44becc08d1123894b6576dfb6cd8a
blee <blee@mitre.org>
2021-09-13 13:16:26 -0400

    update tests

--- tests/api/v2/handlers/test_contacts_api.py
+++ tests/api/v2/handlers/test_contacts_api.py
@@ -13,7 +13,7 @@ def contact_report_data(contact_svc):
                 "date": "2021-08-29 15:00:00"
             }
         ],
-        'html': [
+        'HTML': [
             {
                 "paw": "test_2",
                 "instructions": [],
@@ -26,19 +26,19 @@ def contact_report_data(contact_svc):
 
 class TestContactsApi:
     async def test_get_http_contact_report(self, api_v2_client, api_cookies, contact_report_data):
-        resp = await api_v2_client.get('/api/v2/contacts/http', cookies=api_cookies)
+        resp = await api_v2_client.get('/api/v2/contacts/HTTP', cookies=api_cookies)
         assert resp.status == HTTPStatus.OK
         output = await resp.json()
         assert output == contact_report_data['HTTP']
 
     async def test_get_other_contact_report(self, api_v2_client, api_cookies, contact_report_data):
-        resp = await api_v2_client.get('/api/v2/contacts/html', cookies=api_cookies)
+        resp = await api_v2_client.get('/api/v2/contacts/HTML', cookies=api_cookies)
         assert resp.status == HTTPStatus.OK
         output = await resp.json()
-        assert output == contact_report_data['html']
+        assert output == contact_report_data['HTML']
 
     async def test_unauthorized_get_contact_report(self, api_v2_client, contact_report_data):
-        resp = await api_v2_client.get('/api/v2/contacts/html')
+        resp = await api_v2_client.get('/api/v2/contacts/HTML')
         assert resp.status == HTTPStatus.UNAUTHORIZED
 
     async def test_nonexistent_get_contact_report(self, api_v2_client, api_cookies, contact_report_data):
