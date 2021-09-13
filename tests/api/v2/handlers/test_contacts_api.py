926a77f4adcb9a3d2a6d56913deaca586c708fa8
blee <blee@mitre.org>
2021-09-13 14:13:12 -0400

    test upper/lowercase contact endpoints

--- tests/api/v2/handlers/test_contacts_api.py
+++ tests/api/v2/handlers/test_contacts_api.py
@@ -25,14 +25,14 @@ def contact_report_data(contact_svc):
 
 
 class TestContactsApi:
-    async def test_get_http_contact_report(self, api_v2_client, api_cookies, contact_report_data):
+    async def test_get_uppercase_contact_report(self, api_v2_client, api_cookies, contact_report_data):
         resp = await api_v2_client.get('/api/v2/contacts/HTTP', cookies=api_cookies)
         assert resp.status == HTTPStatus.OK
         output = await resp.json()
         assert output == contact_report_data['HTTP']
 
-    async def test_get_other_contact_report(self, api_v2_client, api_cookies, contact_report_data):
-        resp = await api_v2_client.get('/api/v2/contacts/HTML', cookies=api_cookies)
+    async def test_get_lowercase_contact_report(self, api_v2_client, api_cookies, contact_report_data):
+        resp = await api_v2_client.get('/api/v2/contacts/html', cookies=api_cookies)
         assert resp.status == HTTPStatus.OK
         output = await resp.json()
         assert output == contact_report_data['HTML']
