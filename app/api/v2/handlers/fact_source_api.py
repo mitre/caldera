2c7e012da757f3179b92f84c72adaaec90cd629b
blee <blee@mitre.org>
2021-09-16 12:53:04 -0400

    add delete endpoint for fact sources

--- app/api/v2/handlers/fact_source_api.py
+++ app/api/v2/handlers/fact_source_api.py
@@ -20,6 +20,7 @@ class FactSourceApi(BaseObjectApi):
         router.add_post('/sources', self.create_fact_source)
         router.add_patch('/sources/{id}', self.update_fact_source)
         router.add_put('/sources/{id}', self.create_or_update_source)
+        router.add_delete('/sources/{id}', self.delete_source)
 
     @aiohttp_apispec.docs(tags=['sources'])
     @aiohttp_apispec.querystring_schema(BaseGetAllQuerySchema)
@@ -55,3 +56,9 @@ class FactSourceApi(BaseObjectApi):
     async def create_or_update_source(self, request: web.Request):
         source = await self.create_or_update_on_disk_object(request)
         return web.json_response(source.display)
+
+    @aiohttp_apispec.docs(tags=['sources'])
+    @aiohttp_apispec.response_schema(SourceSchema)
+    async def delete_source(self, request: web.Request):
+        await self.delete_object(request)
+        return web.HTTPNoContent()
