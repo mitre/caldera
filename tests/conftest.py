4da3bdc97ab3489850d3217b08ca42f283d4d2c9
Ben Lee <55265763+bleepbop@users.noreply.github.com>
2021-10-12 17:07:37 -0400

    [VIRTS-2880] Sources v2 API Pytests (#2307)
    * create test file
    
    * add fixtures
    
    * update nested object instantiation
    
    * all tests passing
    
    * add rules to test objects
    
    * mock time for payload
    
    * update parameters, remove unrelated test function

--- tests/conftest.py
+++ tests/conftest.py
@@ -19,6 +19,7 @@ from app.api.v2.handlers.adversary_api import AdversaryApi
 from app.api.v2.handlers.operation_api import OperationApi
 from app.api.v2.handlers.contact_api import ContactApi
 from app.api.v2.handlers.obfuscator_api import ObfuscatorApi
+from app.api.v2.handlers.fact_source_api import FactSourceApi
 from app.api.v2.handlers.planner_api import PlannerApi
 from app.api.v2.handlers.health_api import HealthApi
 from app.objects.c_obfuscator import Obfuscator
@@ -326,6 +327,7 @@ def api_v2_client(loop, aiohttp_client, contact_svc):
         ContactApi(svcs).add_routes(app)
         ObjectiveApi(svcs).add_routes(app)
         ObfuscatorApi(svcs).add_routes(app)
+        FactSourceApi(svcs).add_routes(app)
         PlannerApi(svcs).add_routes(app)
         HealthApi(svcs).add_routes(app)
         return app
