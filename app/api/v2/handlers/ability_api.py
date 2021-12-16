5b0cbad2ebe98ec869c136f61d39b2192b7afdbc
Emmanuelle Vargas-Gonzalez <emmanuelle@mitre.org>
2021-12-16 12:08:34 -0500

    add documentation for api/v2/abilities

--- app/api/v2/handlers/ability_api.py
+++ app/api/v2/handlers/ability_api.py
@@ -22,35 +22,47 @@ class AbilityApi(BaseObjectApi):
         router.add_patch('/abilities/{ability_id}', self.update_ability)
         router.add_delete('/abilities/{ability_id}', self.delete_ability)
 
-    @aiohttp_apispec.docs(tags=['abilities'])
+    @aiohttp_apispec.docs(tags=['abilities'], summary='Get all abilities.',
+                          descriptions='Provides a list of all available abilities.')
     @aiohttp_apispec.querystring_schema(BaseGetAllQuerySchema)
     @aiohttp_apispec.response_schema(AbilitySchema(many=True, partial=True))
     async def get_abilities(self, request: web.Request):
         abilities = await self.get_all_objects(request)
         return web.json_response(abilities)
 
-    @aiohttp_apispec.docs(tags=['abilities'])
+    @aiohttp_apispec.docs(tags=['abilities'], summary='Get an ability.',
+                          description='Provides one ability based on its ability id.')
     @aiohttp_apispec.querystring_schema(BaseGetOneQuerySchema)
     @aiohttp_apispec.response_schema(AbilitySchema(partial=True))
     async def get_ability_by_id(self, request: web.Request):
         ability = await self.get_object(request)
         return web.json_response(ability)
 
-    @aiohttp_apispec.docs(tags=['abilities'], summary='"name", "tactic", and "executors" are all required fields.')
+    @aiohttp_apispec.docs(tags=['abilities'], summary='Creates a new ability.',
+                          description='Creates a new adversary based on the `AbilitySchema`. '
+                                      '"name", "tactic", and "executors" are all required fields.')
     @aiohttp_apispec.request_schema(AbilitySchema)
     @aiohttp_apispec.response_schema(AbilitySchema)
     async def create_ability(self, request: web.Request):
         ability = await self.create_on_disk_object(request)
         return web.json_response(ability.display)
 
-    @aiohttp_apispec.docs(tags=['abilities'], summary='"name", "tactic", and "executors" are all required fields.')
+    @aiohttp_apispec.docs(tags=['abilities'], summary='Creates new or updates an existing ability.',
+                          description='Given an ability based on the `AbilitySchema` if the identifier '
+                                      'matches an existing ability it will update it replacing it with '
+                                      'the contents received on the request. Otherwise a new ability will '
+                                      'be created. "name", "tactic", and "executors" are all required fields.')
     @aiohttp_apispec.request_schema(AbilitySchema(partial=True))
     @aiohttp_apispec.response_schema(AbilitySchema)
     async def create_or_update_ability(self, request: web.Request):
         ability = await self.create_or_update_on_disk_object(request)
         return web.json_response(ability.display)
 
-    @aiohttp_apispec.docs(tags=['abilities'])
+    @aiohttp_apispec.docs(tags=['abilities'], summary='Updates an existing ability.',
+                          description='Given an ability based on the `AbilitySchema` if the identifier '
+                                      'matches an existing ability it will update it replacing it with '
+                                      'the contents received on the request. "name", "tactic", and '
+                                      '"executors" are all required fields.')
     @aiohttp_apispec.request_schema(AbilitySchema(partial=True, exclude=['ability_id',
                                                                          'requirements',
                                                                          'additional_info',
@@ -60,7 +72,8 @@ class AbilityApi(BaseObjectApi):
         ability = await self.update_on_disk_object(request)
         return web.json_response(ability.display)
 
-    @aiohttp_apispec.docs(tags=['abilities'])
+    @aiohttp_apispec.docs(tags=['abilities'], summary='Deletes an ability.',
+                          description='Deletes an existing ability.')
     @aiohttp_apispec.response_schema(AbilitySchema)
     async def delete_ability(self, request: web.Request):
         await self.delete_on_disk_object(request)
