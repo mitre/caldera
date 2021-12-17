ec20f7cd19334ac47b4572f34bd7ed2e8c7e4837
William Booth <18699738+wbooth@users.noreply.github.com>
2021-12-17 14:56:45 -0500

    Update rest_api.py

--- app/api/rest_api.py
+++ app/api/rest_api.py
@@ -45,6 +45,7 @@ class RestApi(BaseWorld):
 
     @template('login.html', status=401)
     async def login(self, request):
+        print('yo')
         return dict()
 
     async def validate_login(self, request):
