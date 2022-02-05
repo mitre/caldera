917a4a1d9d170252939f6f0ddb9a6b574df22189
djlawren <djlawren@umich.edu>
2022-02-04 19:13:58 -0500

    Removed nested from operation description

--- app/api/v2/handlers/operation_api.py
+++ app/api/v2/handlers/operation_api.py
@@ -67,8 +67,8 @@ class OperationApi(BaseObjectApi):
     @aiohttp_apispec.docs(tags=['operations'],
                           summary='Create a new CALDERA operation record',
                           description='Create a new CALDERA operation using the format provided in the '
-                                      '`OperationSchema`. Required nested schema fields are as follows: '
-                                      '"name", "adversary.adversary_id", "planner.planner_id", and "source.id"')
+                                      '`OperationSchema`. Required schema fields are as follows: "name", '
+                                      '"adversary.adversary_id", "planner.planner_id", and "source.id"')
     @aiohttp_apispec.request_schema(OperationSchema)
     @aiohttp_apispec.response_schema(OperationSchema,
                                      description='The response is the newly-created operation report.')
