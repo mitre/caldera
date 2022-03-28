b6f4c09de868d21c51c5d2e8a13ceff220e5b785
Anna Lin <alin@mitre.org>
2022-03-28 19:29:44 -0400

    lighthouse checks

---
+++ lighthouserc.js
@@ -0,0 +1,11 @@
+module.exports = {
+    ci: {
+      collect: {
+        url: ['http://localhost:8888/'],
+        startServerCommand: ' python3 server.py --insecure --fresh -l DEBUG',
+      },
+      upload: {
+        target: 'temporary-public-storage',
+      },
+    },
+  };
