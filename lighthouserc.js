4f55fd5c65b9afd7edf07132019846f6ef20d5d0
Anna Lin <alin@mitre.org>
2022-04-05 14:57:51 -0400

    Update lighthouserc.js

--- lighthouserc.js
+++ lighthouserc.js
@@ -1,12 +1,10 @@
 module.exports = {
     ci: {
-        assert: {
-            preset: 'lighthouse:recommended',
+        assertions: {
+            "categories:accessibility": ["error", {"minScore": 0.9}]
         },
         collect: {
-            staticDistDir: './templates'
-            url: ['http://localhost:8888/'],
-            startServerCommand: ' python3 server.py --insecure --fresh -l DEBUG',
+            staticDistDir: './templates',
         },
         upload: {
             target: 'temporary-public-storage',
