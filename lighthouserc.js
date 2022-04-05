44b051b72627c70b9a9d5ad74d3126f04f6eeafd
Anna Lin <alin@mitre.org>
2022-04-05 15:25:41 -0400

    Update lighthouserc.js

--- lighthouserc.js
+++ lighthouserc.js
@@ -1,8 +1,15 @@
 module.exports = {
     ci: {
-        assertions: {
-            "categories:accessibility": ["error", {"minScore": 0.9}]
-        },
+        assert: {
+            "assertMatrix": [
+                {
+                  "matchingUrlPattern": ".*",
+                  "assertions": {
+                    "categories:accessibility": ["error", {"minScore": 0.9}]
+                  }
+                }
+            ],
+        }
         collect: {
             numberOfRuns: 1,
             maxAutodiscoverUrls: 0,
