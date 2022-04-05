960776dcd98bfecdd176d99d97eab44b343bf9c6
Anna Lin <alin@mitre.org>
2022-04-05 15:27:01 -0400

    Update lighthouserc.js

--- lighthouserc.js
+++ lighthouserc.js
@@ -9,7 +9,7 @@ module.exports = {
                   }
                 }
             ],
-        }
+        },
         collect: {
             numberOfRuns: 1,
             maxAutodiscoverUrls: 0,
