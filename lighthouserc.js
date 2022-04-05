2e0dfe53d7e7ee670115a9592770a336221a526b
Anna Lin <alin@mitre.org>
2022-04-05 18:03:07 -0400

    Try one at a time

--- lighthouserc.js
+++ lighthouserc.js
@@ -15,7 +15,7 @@ module.exports = {
             ],
         },
         collect: {
-            numberOfRuns: 11,
+            numberOfRuns: 12,
             maxAutodiscoverUrls: 1,
             staticDistDir: './templates'
         },
