a0b40ed5c47ac9be439f44088b962998e51cc4e2
Michael Jones <michaeljones@mitre.org>
2022-10-18 18:38:47 -0500

    Fixed styling issues in link_result_schema.py

--- app/api/v2/schemas/link_result_schema.py
+++ app/api/v2/schemas/link_result_schema.py
@@ -1,7 +1,9 @@
 from app.objects.secondclass.c_link import LinkSchema
 
+
 import marshmallow as ma
 from marshmallow import fields
+
 
 class LinkResultSchema(ma.Schema):
     link = fields.Nested(LinkSchema)
