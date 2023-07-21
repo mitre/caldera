fd180c16faa3f7b75d606f31f5727cfed919bde6
Matthew Rogers <matthewrogers@mitre.org>
2023-07-21 11:43:01 -0400

    flake8 initalize_plugin_facts.py

--- initialize_plugin_facts.py
+++ initialize_plugin_facts.py
@@ -4,23 +4,18 @@ import os
 from pathlib import Path
 
 
-
-
 def get_fact_descriptions():
     ability_files = glob.glob("./plugins/**/data/abilities/**/*.yml")
     facts_per_plugin = {}
     for ability_path in ability_files:
-
         with open(ability_path, "r") as file:
             plugin_name = Path(ability_path).parents[3].name
             if plugin_name not in facts_per_plugin:
-                # print("reseting")
                 facts_per_plugin[plugin_name] = set()
             r_ability = yaml.safe_load(file)[0]
             commands = []
             if "executors" in r_ability:
-                #todo - look for multiple executors
-                command = r_ability["executors"][0]["command"]
+                commands.append(r_ability["executors"][0]["command"])
             else:
                 for platform in r_ability["platforms"]:
                     try:
@@ -31,11 +26,11 @@ def get_fact_descriptions():
                     except KeyError:
                         pass
                 if not commands:
-                    #No command
                     continue
         plugin_facts = get_facts_from_commands(commands, plugin_name)
         facts_per_plugin[plugin_name] = facts_per_plugin[plugin_name] | plugin_facts[plugin_name]
     return facts_per_plugin
+
 
 def get_facts_from_commands(commands, plugin_name):
     plugin_facts = {plugin_name: set()}
@@ -45,17 +40,14 @@ def get_facts_from_commands(commands, plugin_name):
             if "#{" in cmd:
                 fact_start_idx = cmd.index("#{")
                 fact_end_idx = cmd.index("}")
-                fact_name = cmd[fact_start_idx: fact_end_idx + 1]
                 unprocessed_fact_name = cmd[fact_start_idx + 2: fact_end_idx]
-                fact_start = cmd[0:fact_start_idx]
-                fact_end = cmd[fact_end_idx + 1:]
                 plugin_facts[plugin_name].add(unprocessed_fact_name)
-    return(plugin_facts)
+    return (plugin_facts)
+
 
 def write_descriptions(facts_per_plugin):
     for plug in facts_per_plugin:
-        descriptions = {x:{"default":None, "description":""} for x in facts_per_plugin[plug]}
-        #Verify that we aren't overwriting files, default to using existing files and add new facts
+        descriptions = {x: {"default": None, "description": ""} for x in facts_per_plugin[plug]}
         descriptions_path = f"./plugins/{plug}/fact_description.yml"
         to_write = {}
         if os.path.exists(descriptions_path):
@@ -65,7 +57,7 @@ def write_descriptions(facts_per_plugin):
                     to_write = existing_data
                     for k in descriptions:
                         if k not in existing_data:
-                            to_write.update({k:descriptions[k]})
+                            to_write.update({k: descriptions[k]})
         else:
             to_write = descriptions
 
