66ced4b3a7ce61bfff272605f6ab2096e7f75e3a
Chris Lenk <clenk@mitre.org>
2024-02-07 13:24:54 -0500

    Add payload handler tests

---
+++ tests/api/v2/handlers/test_payloads_api.py
@@ -0,0 +1,19 @@
+import pytest
+
+from http import HTTPStatus
+
+from app.utility.base_service import BaseService
+
+
+
+class TestPayloadsApi:
+    async def test_get_payloads(self, api_v2_client, api_cookies):
+        resp = await api_v2_client.get('/api/v2/payloads', cookies=api_cookies)
+        payloads_list = await resp.json()
+        assert len(payloads_list) > 0
+        payload = payloads_list[0]
+        assert type(payload) == str
+
+    async def test_unauthorized_get_payloads(self, api_v2_client):
+        resp = await api_v2_client.get('/api/v2/payloads')
+        assert resp.status == HTTPStatus.UNAUTHORIZED
