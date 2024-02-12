75e16a1829dec6b8d03677ecb10eded6f8bf1913
elegantmoose <mkouremetis16@gmail.com>
2024-02-12 11:38:55 -0500

    ascii banner adjustments

--- app/ascii_banner.py
+++ app/ascii_banner.py
@@ -1,5 +1,10 @@
-RED = "\33[91m"
+import os
+
+
+RED = "\33[1m"
+DARK_RED = "\x1b[38;5;1m"
 BLUE = "\33[94m"
+DARK_BLUE = "\x1b[38;5;20m"
 GREEN = "\033[32m"
 YELLOW = "\033[93m"
 PURPLE = '\033[0;35m'
@@ -18,4 +23,25 @@ _BANNER = """
 """
 
 
-ASCII_BANNER = f"{DARK_PURPLE} {_BANNER} {END}"
+_BANNER_SECTION_1 =  "\n\
+ ██████╗ █████╗ ██╗     ██████╗ ███████╗██████╗  █████╗\n\
+██╔════╝██╔══██╗██║     ██╔══██╗██╔════╝██╔══██╗██╔══██╗\n\
+"
+
+
+_BANNER_SECTION_2 = "\
+██║     ███████║██║     ██║  ██║█████╗  ██████╔╝███████║\n\
+██║     ██╔══██║██║     ██║  ██║██╔══╝  ██╔══██╗██╔══██║\n\
+"
+
+
+BANNER_SECTION_3 = "\
+╚██████╗██║  ██║███████╗██████╔╝███████╗██║  ██║██║  ██║\n\
+ ╚═════╝╚═╝  ╚═╝╚══════╝╚═════╝ ╚══════╝╚═╝  ╚═╝╚═╝  ╚═╝\n\
+"
+
+
+if int(os.environ['NO_COLOR']) == 1:
+    ASCII_BANNER = _BANNER
+else:
+    ASCII_BANNER = f"{DARK_BLUE}{_BANNER_SECTION_1}{DARK_PURPLE}{_BANNER_SECTION_2}{DARK_RED}{BANNER_SECTION_3}{END}"
