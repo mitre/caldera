e4fe33bd78be47fe6161ab65cbc3f504e0837909
jean-baptiste-perez-bib <jean-baptiste.perez_ext@michelin.com>
2024-06-07 21:20:23 +0200

    Adds API v2 POST payloads

--- app/api/v2/handlers/payload_api.py
+++ app/api/v2/handlers/payload_api.py
@@ -1,5 +1,8 @@
+import asyncio
 import itertools
+import os
 import pathlib
+from io import IOBase
 
 import aiohttp_apispec
 from aiohttp import web
@@ -16,6 +19,9 @@ class PayloadQuerySchema(schema.Schema):
 class PayloadSchema(schema.Schema):
     payloads = fields.List(fields.String())
 
+class PayloadCreateRequestSchema(schema.Schema):
+    file = fields.Raw(type="file", required=True)
+
 
 class PayloadApi(BaseApi):
     def __init__(self, services):
@@ -26,6 +32,7 @@ class PayloadApi(BaseApi):
     def add_routes(self, app: web.Application):
         router = app.router
         router.add_get('/payloads', self.get_payloads)
+        router.add_post("/payloads", self.post_payloads)
 
     @aiohttp_apispec.docs(tags=['payloads'],
                           summary='Retrieve payloads',
@@ -58,3 +65,49 @@ class PayloadApi(BaseApi):
             payloads.sort()
 
         return web.json_response(payloads)
+
+    @aiohttp_apispec.docs(
+        tags=['payloads'],
+        summary='Create a payload',
+        description='Uploads a payload.')
+    @aiohttp_apispec.form_schema(PayloadCreateRequestSchema)
+    @aiohttp_apispec.response_schema(
+        PayloadSchema(),
+        description="The created payload in a list in PayloadSchema format (with name changed in case of a duplicate).")
+    async def post_payloads(self, request: web.Request):
+        # As aiohttp_apispec.form_schema already calls request.multipart(),
+        # accessing the file using the prefilled request["form"] dictionary.
+        file_field: web.FileField = request["form"]["file"]
+
+        file_name_candidate: str = file_field.filename
+        file_path: str = os.path.join('data/payloads/', file_name_candidate)
+        suffix: int = 1
+
+        # Generating a file suffix in the case it already exists.
+        while os.path.exists(file_path):
+            file_name_candidate = f"{pathlib.Path(file_field.filename).stem}_" \
+                                  f"{suffix}{pathlib.Path(file_field.filename).suffix}"
+            file_path = os.path.join('data/payloads/', file_name_candidate)
+            suffix += 1
+
+        file_name: str = file_name_candidate
+
+        # The file_field.file is of type IOBase: It uses blocking methods.
+        # Putting blocking code into a dedicated method and thread...
+        def save_file(target_file_path: str, io_base_src: IOBase):
+            size: int = 0
+            read_chunk: bool = True
+            with open(target_file_path, 'wb') as buffered_io_base_dest:
+                while read_chunk:
+                    chunk: bytes = io_base_src.read(8192)
+                    if chunk:
+                        size += len(chunk)
+                        buffered_io_base_dest.write(chunk)
+                    else:
+                        read_chunk = False
+
+        loop: asyncio.AbstractEventLoop = asyncio.get_event_loop()
+        await loop.run_in_executor(None, save_file, file_path, file_field.file)
+
+        body: dict[list[str]] = {"payloads": [file_name]}
+        return web.json_response(body)
