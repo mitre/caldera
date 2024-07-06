a4cd739ae1faf7f2b8c7ba531f1d088509a4b7a9
jean-baptiste-perez-bib <jean-baptiste.perez_ext@michelin.com>
2024-07-06 22:11:56 +0200

    Fixes a broken test (test_health_api.py)
    Method TestHealthApi.test_unauthorized_get_health.
    Empty access table was not taken into account.

--- tests/api/v2/handlers/test_health_api.py
+++ tests/api/v2/handlers/test_health_api.py
@@ -1,3 +1,5 @@
+import copy
+
 import pytest
 import app
 
@@ -14,6 +16,13 @@ def expected_caldera_info():
     }
 
 
+@pytest.fixture
+def expected_unauthorized_caldera_info(expected_caldera_info):
+    new_info = copy.deepcopy(expected_caldera_info)
+    new_info['access'] = None
+    return new_info
+
+
 class TestHealthApi:
     async def test_get_health(self, api_v2_client, api_cookies, expected_caldera_info):
         resp = await api_v2_client.get('/api/v2/health', cookies=api_cookies)
@@ -21,8 +30,8 @@ class TestHealthApi:
         output_info = await resp.json()
         assert output_info == expected_caldera_info
 
-    async def test_unauthorized_get_health(self, api_v2_client, expected_caldera_info):
+    async def test_unauthorized_get_health(self, api_v2_client, expected_unauthorized_caldera_info):
         resp = await api_v2_client.get('/api/v2/health')
         assert resp.status == HTTPStatus.OK
         output_info = await resp.json()
-        assert output_info == expected_caldera_info
+        assert output_info == expected_unauthorized_caldera_info
