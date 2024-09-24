5df1ba30276ebb2059d4769fd21a8c943df78ff6
jean-baptiste-perez-bib <jean-baptiste.perez_ext@michelin.com>
2024-09-24 21:08:09 +0200

    Updates /api/v2/health endpoint
    Makes it require authenticated users.
    Simplifies back the management of the returned "access" field.

--- tests/api/v2/handlers/test_health_api.py
+++ tests/api/v2/handlers/test_health_api.py
@@ -1,5 +1,3 @@
-import copy
-
 import pytest
 import app
 
@@ -16,13 +14,6 @@ def expected_caldera_info():
     }
 
 
-@pytest.fixture
-def expected_unauthorized_caldera_info(expected_caldera_info):
-    new_info = copy.deepcopy(expected_caldera_info)
-    new_info['access'] = None
-    return new_info
-
-
 class TestHealthApi:
     async def test_get_health(self, api_v2_client, api_cookies, expected_caldera_info):
         resp = await api_v2_client.get('/api/v2/health', cookies=api_cookies)
@@ -30,8 +21,6 @@ class TestHealthApi:
         output_info = await resp.json()
         assert output_info == expected_caldera_info
 
-    async def test_unauthorized_get_health(self, api_v2_client, expected_unauthorized_caldera_info):
+    async def test_unauthorized_get_health(self, api_v2_client):
         resp = await api_v2_client.get('/api/v2/health')
-        assert resp.status == HTTPStatus.OK
-        output_info = await resp.json()
-        assert output_info == expected_unauthorized_caldera_info
+        assert resp.status == HTTPStatus.UNAUTHORIZED
