099f11ee5ef5dc68dc933f036cd60ab92fdb76a8
kmpepper <kpepper@mitre.org>
2026-01-09 19:02:35 +0000

    Moving plain text and base64 obfuscators to Caldera core to remove Stockpile dependencies

--- server.py
+++ server.py
@@ -98,6 +98,20 @@ def run_tasks(services, run_vue_server=False):
     loop.create_task(app_svc.watch_ability_files())
     loop.run_until_complete(start_server())
     loop.run_until_complete(event_svc.fire_event(exchange="system", queue="ready"))
+        loop.run_until_complete(
+        data_svc.store(
+            Obfuscator(name='plain-text',
+                    description='Does no obfuscation to any command, instead running it in plain text',
+                    module='app.obfuscators.plain_text')
+        )
+    )
+    loop.run_until_complete(
+        data_svc.store(
+            Obfuscator(name='base64',
+                    description='Obfuscates commands in base64',
+                    module='app.obfuscators.base64_basic')
+        )
+    )
     if run_vue_server:
         loop.run_until_complete(start_vue_dev_server())
     try:
