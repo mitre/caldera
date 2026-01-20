1831020313efdd60daca8c59714332d768c3575e
Joshua Klosterman <jklosterman@mitre.org>
2026-01-20 10:18:09 -0500

    Added to test_contact_tcp.py

--- tests/contacts/test_contact_tcp.py
+++ tests/contacts/test_contact_tcp.py
@@ -1,6 +1,7 @@
 import logging
 import socket
 from unittest import mock
+import time
 
 from app.utility.base_world import BaseWorld
 from app.contacts.contact_tcp import TcpSessionHandler
@@ -45,5 +46,31 @@ class TestContact:
         BaseWorld.set_config('main', 'app.contact.tcp', '127.0.0.1:57012')
         self.services = app_svc.get_services()
         tcp_c2 = Contact(services=self.services)
+        tcp_c2.tcp_handler.sessions = [
+            mock.Mock(),
+            mock.Mock(),
+            mock.Mock()
+        ]
+        event_loop.run_until_complete(tcp_c2.start())
+        tcp_c2.tcp_handler.accept = mock.AsyncMock()
+        tcp_c2.tcp_handler.send = mock.AsyncMock(return_value=(200, None, b'response', time.time()))
+        tcp_c2.contact_svc.handle_heartbeat = mock.AsyncMock(return_value=(None, []))
+        tcp_c2.tcp_handler.refresh = mock.AsyncMock()
+        tcp_c2.operation_loop = mock.AsyncMock()
+        tcp_c2.tcp_handler._attempt_connection = mock.AsyncMock()
+        event_loop.run_until_complete(tcp_c2.start())
+        assert tcp_c2 is not None
+
+    def test_tcp_contact_errors(self, event_loop, app_svc):
+        BaseWorld.set_config('main', 'app.contact.tcp', '127.0.0.1:57012')
+        self.services = app_svc.get_services()
+        tcp_c2 = Contact(services=self.services)
+        session_with_socket_error = mock.Mock()
+        session_with_socket_error.write_bytes.side_effect = socket.error()
+        tcp_c2.tcp_handler.sessions = [
+            session_with_socket_error,
+            session_with_socket_error,
+            mock.Mock()
+        ]
         event_loop.run_until_complete(tcp_c2.start())
         assert tcp_c2 is not None
