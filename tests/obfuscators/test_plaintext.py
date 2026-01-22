1b1c8155848eb5a197ba5373132c9f1e5cf4b828
kmpepper <kpepper@mitre.org>
2026-01-22 18:37:01 +0000

    Adding unit tests for plain text and b64 basic obfuscators

---
+++ tests/obfuscators/test_plaintext.py
@@ -0,0 +1,20 @@
+import pytest
+
+from base64 import b64encode, b64decode
+from app.objects.secondclass.c_link import Link
+from app.obfuscators.plain_text import Obfuscation
+
+
+@pytest.fixture
+def plaintext_obfuscator(test_agent):
+    return Obfuscation(test_agent)
+
+
+class TestPlainTextObfuscator:
+    def test_plaintext_obfuscator(self, plaintext_obfuscator, finished_link):
+        link = Link.load(finished_link)
+        plaintext_cmd = b64decode(link.command).decode('utf-8')
+        decoded = plaintext_obfuscator.run(link)
+        assert decoded == plaintext_cmd
+        
+
