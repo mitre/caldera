22346416b2d15ea5dfd158bff84d20f857ff4455
Joshua Klosterman <jklosterman@mitre.org>
2026-02-02 11:59:26 -0500

    Added test for handle_sessions function within Contact class

--- tests/contacts/test_contact_tcp.py
+++ tests/contacts/test_contact_tcp.py
@@ -4,12 +4,31 @@ from unittest import mock
 import pytest
 
 
+from app.service.contact_svc import ContactService
 from app.utility.base_world import BaseWorld
 from app.contacts.contact_tcp import TcpSessionHandler
 from app.contacts.contact_tcp import Contact
 from app.contacts.utility.c_tcp_session import TCPSession
+from app.objects.secondclass.c_instruction import Instruction
 
 logger = logging.getLogger(__name__)
+
+
+@pytest.fixture
+def tcp_c2(app_svc, contact_svc, data_svc, obfuscator):
+    services = app_svc.get_services()
+    tcp_contact_svc = Contact(services=services)
+    return tcp_contact_svc
+
+
+class _MockReader:
+    async def read(self, n=-1):
+        return b'MockContent'
+
+
+class _MockWriter:
+    def write(self, data):
+        pass
 
 
 class TestTcpSessionHandler:
@@ -41,37 +60,33 @@ class TestTcpSessionHandler:
         event_loop.run_until_complete(handler.refresh())
         assert len(handler.sessions) == 3
 
-
-@pytest.fixture
-def tcp_c2(app_svc, contact_svc, data_svc, obfuscator):
-    services = app_svc.get_services()
-    tcp_contact_svc = Contact(services=services)
-    return tcp_contact_svc
-
-
-class _MockReader:
-    async def read(self, n=-1):
-        return b'MockContent'
-
-
-class _MockWriter:
-    def write(self, data):
-        pass
+    async def test_attempt_connection(self, tcp_c2):
+        MockSession = TCPSession(id=123456, paw='testpaw', reader=_MockReader(), writer=_MockWriter())
+        assert "MockContent" == await tcp_c2.tcp_handler._attempt_connection(MockSession, timeout=1)
 
 
 class TestContact:
 
     def test_tcp_contact(self, event_loop, tcp_c2):
         BaseWorld.set_config('main', 'app.contact.tcp', '127.0.0.1:57012')
-        tcp_c2.tcp_handler.sessions = [
-            mock.Mock(),
-            mock.Mock(),
-            mock.Mock()
-        ]
+        dummy_instruction = Instruction(
+            id='123',
+            sleep=5,
+            command='whoami',
+            executor='sh',
+            timeout=60,
+            payloads=[],
+            uploads=[],
+            deadman=False,
+            delete_payload=True
+        )
+        tcp_c2.tcp_handler.sessions.append(TCPSession(
+            id=1,
+            paw='dummy_paw',
+            reader=_MockReader(),
+            writer=_MockWriter()
+        ))
         event_loop.run_until_complete(tcp_c2.start())
-        event_loop.run_until_complete(tcp_c2.tcp_handler.accept(None, None))
-        assert tcp_c2 is not None
-
-    async def test_attempt_connection(self, tcp_c2):
-        MockSession = TCPSession(id=123456, paw='testpaw', reader=_MockReader(), writer=_MockWriter())
-        assert "MockContent" == await tcp_c2.tcp_handler._attempt_connection(MockSession, timeout=1)
+        with mock.patch.object(ContactService, 'handle_heartbeat', return_value=('dummy_paw', [dummy_instruction])):
+            event_loop.run_until_complete(tcp_c2.handle_sessions())
+        assert len(tcp_c2.tcp_handler.sessions) == 1
